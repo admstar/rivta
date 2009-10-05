8623f7f9c4ab8a5ea32bf11c04e422143ff09367
johaneltes <johaneltes@c1faef40-eec5-6430-f181-9cb8a2d1d0ae>
2009-10-05 19:26:15 +0000

    Renamed wsdl according to changed naming rule

--- src/bp20/refapp/trunk/microsoft/wcf/rivta-bp20-refapp/rivta-bp20-refapp-schemas/generate-src.bat
+++ src/bp20/refapp/trunk/microsoft/wcf/rivta-bp20-refapp/rivta-bp20-refapp-schemas/generate-src.bat
@@ -7,10 +7,10 @@
 @REM Problem with adressing the wsdl locally. Workaround in place is to use the Java - Apache CXF Web Service to provide the wsdl.
 @REM 
 @REM The command should look like:
-@REM 	svcutil.exe /language:cs /out:EhrExtractionResponderProxy.cs /config:app.config business\JournalinfoApoteketRIV\EhrExtractionInteraction.wsdl business\JournalinfoApoteketRIV\*.xsd  technical\wsaddressing\ws-addressing-1.0.xsd
+@REM 	svcutil.exe /language:cs /out:EhrExtractionResponderProxy.cs /config:app.config business\JournalinfoApoteketRIV\EhrExtractionInteraction_1.1_rivtabp20.wsdl business\JournalinfoApoteketRIV\*.xsd  technical\wsaddressing\ws-addressing-1.0.xsd
 @REM 
 @REM But gives us errormessages like
 @REM 	Validation Error: Type 'urn:riv13606:v1:II' is not declared.
 @REM 
 @REM --------------------------------------------------------------------------
-svcutil.exe /language:cs /out:generated-src\EhrExtractionResponderProxy.cs /config:generated-src\app.config /namespace:urn:riv:ehr:ehrexchange:EhrExtraction:1:rivtabp20,rivta_bp20_refapp_schemas.wsdl /namespace:urn:riv:ehr:ehrexchange:EhrExtraction:1,rivta_bp20_refapp_schemas.xsd business\JournalinfoApoteketRIV\EhrExtractionInteraction.wsdl business\JournalinfoApoteketRIV\*.xsd  technical\wsaddressing\ws-addressing-1.0.xsd
+svcutil.exe /language:cs /out:generated-src\EhrExtractionResponderProxy.cs /config:generated-src\app.config /namespace:urn:riv:ehr:ehrexchange:EhrExtraction:1:rivtabp20,rivta_bp20_refapp_schemas.wsdl /namespace:urn:riv:ehr:ehrexchange:EhrExtraction:1,rivta_bp20_refapp_schemas.xsd business\JournalinfoApoteketRIV\EhrExtractionInteraction_1.1_rivtabp20.wsdl business\JournalinfoApoteketRIV\*.xsd  technical\wsaddressing\ws-addressing-1.0.xsd
