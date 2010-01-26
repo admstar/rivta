1db940453e1a8e99efef67ea3511b1b9a152ee70
danielberggren <danielberggren@c1faef40-eec5-6430-f181-9cb8a2d1d0ae>
2010-01-26 15:43:36 +0000

    Ändrade scriptfil för att bygga mot Axis

--- ServiceInteractions/riv/crm/carelisting/trunk/script/generate-src-with-axis.cmd
+++ ServiceInteractions/riv/crm/carelisting/trunk/script/generate-src-with-axis.cmd
@@ -12,9 +12,21 @@ set AXIS2_CLASS_PATH=%AXIS2_HOME%
 FOR %%c in ("%AXIS2_HOME%\lib\*.jar") DO set AXIS2_CLASS_PATH=!AXIS2_CLASS_PATH!;%%c
 
 @rem ======================================================================
+
+CALL wsdl2java -g -uri ..\tjanstekontrakt\CreateListningInteraction.wsdl
 CALL wsdl2java -g -uri ..\tjanstekontrakt\GetAvailableFacilitiesInteraction.wsdl
+CALL wsdl2java -g -uri ..\tjanstekontrakt\GetListningInteraction.wsdl
+CALL wsdl2java -g -uri ..\tjanstekontrakt\GetListningTypesInteraction.wsdl
+CALL wsdl2java -g -uri ..\tjanstekontrakt\GetPersonQueueStatusInteraction.wsdl
+
+CALL javac -cp .;%AXIS2_CLASS_PATH%  src\rivtabp20\_1\createlisting\carelisting\crm\riv\*.java
 CALL javac -cp .;%AXIS2_CLASS_PATH%  src\rivtabp20\_1\getavailablefacilities\carelisting\crm\riv\*.java
+CALL javac -cp .;%AXIS2_CLASS_PATH%  src\rivtabp20\_1\getlisting\carelisting\crm\riv\*.java
+CALL javac -cp .;%AXIS2_CLASS_PATH%  src\rivtabp20\_1\getlistingtypes\carelisting\crm\riv\*.java
+CALL javac -cp .;%AXIS2_CLASS_PATH%  src\rivtabp20\_1\getpersonqueuestatus\carelisting\crm\riv\*.java
+
+
 cd  src
-jar -cf ..\..\lib\axis1.4.1_With_jdk5_0_19_listingservice.jar rivtabp20\_1\getavailablefacilities\carelisting\crm\riv
+jar -cf ..\..\lib\axis1.4.1_With_jdk5_0_19_listingservice.jar rivtabp20\_1\createlisting\carelisting\crm\riv rivtabp20\_1\getavailablefacilities\carelisting\crm\riv rivtabp20\_1\getlisting\carelisting\crm\riv rivtabp20\_1\getlistingtypes\carelisting\crm\riv rivtabp20\_1\getpersonqueuestatus\carelisting\crm\riv 
 cd ..
-rmdir /S /Q src
+rem rmdir /S /Q src
