cf5abdb09c6ee568ed563f04cb5cf564bc89eff7
johaneltes <johaneltes@c1faef40-eec5-6430-f181-9cb8a2d1d0ae>
2010-02-09 12:25:16 +0000

    New profile added

---
+++ src/bpip20/refapp/trunk/java/cxf/rivta-bpip20-refapp-consumer/src/main/java/se/skl/rivta/bpip20/refapp/consumer/ehrextraction/Initiator.java
@@ -0,0 +1,156 @@
+/**
+ * Licensed to the Apache Software Foundation (ASF) under one
+ * or more contributor license agreements. See the NOTICE file
+ * distributed with this work for additional information
+ * regarding copyright ownership. The ASF licenses this file
+ * to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License. You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied. See the License for the
+ * specific language governing permissions and limitations
+ * under the License.
+ */
+
+package se.skl.rivta.bpip20.refapp.consumer.ehrextraction;
+
+import java.net.MalformedURLException;
+import java.net.URL;
+import java.util.List;
+
+import oasis.names.tc.saml._2_0.assertion.AssertionType;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.w3.wsaddressing10.AttributedURIType;
+
+import se.skl.riv.ehr.ehrexchange.getehrextract.v1.rivtabpip20.GetEhrExtractResponderInterface;
+import se.skl.riv.ehr.ehrexchange.getehrextract.v1.rivtabpip20.GetEhrExtractResponderService;
+import se.skl.riv.ehr.ehrexchange.getehrextractresponder.v1.GetEhrExtractRequestType;
+import se.skl.riv.ehr.ehrexchange.getehrextractresponder.v1.GetEhrExtractResponseType;
+import se.skl.riv.ehr.ehrexchange.getehrextractresponder.v1.PingRequestType;
+import se.skl.riv.ehr.ehrexchange.getehrextractresponder.v1.PingResponseType;
+import se.skl.riv13606.v2.EHREXTRACT;
+import se.skl.riv13606.v2.II;
+import se.skl.rivta.bp20.refapp.util.Util;
+
+public class Initiator {
+
+	static private final Logger logger = LoggerFactory.getLogger(Util.class);
+
+	static private final String INFO_MSG = "RIV TA BP2.0 Ref App OK";
+	static private final String LOGICAL_ADDRESS = "SE165565968202-3P3Q"; // HSA-id for Carelink AB, assuming the responder service i NPO frage-tjanst	
+	static private final AssertionType HP_ASSERTION = new AssertionType();
+	
+	private GetEhrExtractResponderInterface service = null;
+
+	public static void main(String[] args) {
+		
+		// Make CXF use log4j (instead of JDK-logging), currently can't use slf4j
+		System.setProperty("org.apache.cxf.Logger", "org.apache.cxf.common.logging.Log4jLogger");
+
+		// Parse main arguments
+		String address = null;
+		if (args.length == 0) {
+			// Default address
+			address = Util.getProperty("cxf.url");
+		} else {
+			// Use the provided URL, note that this address has to be using the https - protocoll as defined in cxf.xml, i.e.: <http:conduit name="...">
+			address = args[0];			
+		}
+
+        Initiator initiator = new Initiator(address);
+
+		long ts = 0;
+		logger.info("RIV TA Basic Profile med intygspropagering v2.0 - Ref App, Apache CXF Consumer running on Java version {}", System.getProperty("java.version"));
+		logger.info("...connecting to {}", address);
+
+		logger.info("Calling ping-operation first time...");
+		ts = System.currentTimeMillis();
+		PingResponseType pingReply = initiator.callPing(INFO_MSG, LOGICAL_ADDRESS);
+		logger.info("...Producer returned (in {} ms): {}/{}", new Object[] {(System.currentTimeMillis() - ts), pingReply.getLogicalAddress(), pingReply.getInfo()});
+
+		logger.info("Calling ping-operation second time...");
+		ts = System.currentTimeMillis();
+		pingReply = initiator.callPing(INFO_MSG, LOGICAL_ADDRESS);
+		logger.info("...Producer returned (in {} ms): {}/{}", new Object[] {(System.currentTimeMillis() - ts), pingReply.getLogicalAddress(), pingReply.getInfo()});
+
+		logger.info("Calling getEhrExtract-operation first time...");
+		ts = System.currentTimeMillis();
+		GetEhrExtractResponseType reply = initiator.callGetEhrExtract("RIV TA BP IP 2.0 Ref App OK", LOGICAL_ADDRESS);
+		logger.info("...Producer returned (in {} ms): ", (System.currentTimeMillis() - ts), initiator.getIdentifierName(reply));
+
+		logger.info("Calling getEhrExtract-operation second time...");
+		ts = System.currentTimeMillis();
+		reply = initiator.callGetEhrExtract("RIV TA BP IP 2.0 Ref App OK", LOGICAL_ADDRESS);
+		logger.info("...Producer returned (in {} ms): ", (System.currentTimeMillis() - ts), initiator.getIdentifierName(reply));
+	}
+
+	public Initiator(String address) {
+		// Setup ssl info for the initial ?wsdl lookup...
+		setupSsl();
+
+		service = new GetEhrExtractResponderService(createEndpointUrlFromServiceAddress(address)).getGetEhrExtractResponderPort();
+	}
+
+	/**
+	 * Setup ssl info for the initial ?wsdl lookup...
+	 */
+	private void setupSsl() {
+		System.setProperty("javax.net.ssl.keyStore","../certs/consumer.jks");
+		System.setProperty("javax.net.ssl.keyStorePassword", "password");
+		System.setProperty("javax.net.ssl.trustStore", "../certs/consumer-truststore.jks");
+		System.setProperty("javax.net.ssl.trustStorePassword", "password");
+	}
+
+	public GetEhrExtractResponseType callGetEhrExtract(String id, String logicalAddresss) {
+		
+		AttributedURIType logicalAddressHeader = createLogicalAddressHeader(logicalAddresss);
+
+		GetEhrExtractRequestType request = new GetEhrExtractRequestType();
+		II ii = new II();
+		ii.setRoot(id);
+		request.setSubjectOfCareId(ii);
+		
+		GetEhrExtractResponseType result = service.getEhrExtract(logicalAddressHeader, HP_ASSERTION, request);
+
+		return result;
+	}
+
+	public PingResponseType callPing(String info, String logicalAddresss) {
+		
+		AttributedURIType logicalAddressHeader = createLogicalAddressHeader(logicalAddresss);
+
+		PingRequestType request = new PingRequestType();
+		request.setInfo(info);
+		
+		PingResponseType result = service.ping(logicalAddressHeader, request);
+
+		return result;
+	}
+
+	private URL createEndpointUrlFromServiceAddress(String serviceAddress) {
+		try {
+			return new URL(serviceAddress + "?wsdl");
+		} catch (MalformedURLException e) {
+			throw new RuntimeException("Malformed URL Exception: " + e.getMessage());
+		}
+	}
+
+	private AttributedURIType createLogicalAddressHeader(String logicalAddresss) {
+		AttributedURIType logicalAddressHeader = new AttributedURIType();
+		logicalAddressHeader.setValue(logicalAddresss);
+		return logicalAddressHeader;
+	}
+
+	private String getIdentifierName(GetEhrExtractResponseType result) {
+		List<EHREXTRACT> list = result.getEhrExtract();
+		EHREXTRACT firstExtract = list.get(0);
+		return firstExtract.getSubjectOfCare().getIdentifierName();
+	}
+}
---
+++ src/bpip20/refapp/trunk/java/cxf/rivta-bpip20-refapp-producer/src/main/java/se/skl/rivta/bp20/refapp/producer/ehrextraction/Responder.java
@@ -0,0 +1,127 @@
+/**
+ * Licensed to the Apache Software Foundation (ASF) under one
+ * or more contributor license agreements. See the NOTICE file
+ * distributed with this work for additional information
+ * regarding copyright ownership. The ASF licenses this file
+ * to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License. You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied. See the License for the
+ * specific language governing permissions and limitations
+ * under the License.
+ */
+
+package se.skl.rivta.bp20.refapp.producer.ehrextraction;
+
+import java.util.Date;
+
+import javax.jws.WebService;
+
+import oasis.names.tc.saml._2_0.assertion.AssertionType;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.w3.wsaddressing10.AttributedURIType;
+
+import se.skl.riv.ehr.ehrexchange.getehrextract.v1.rivtabpip20.GetEhrExtractResponderInterface;
+import se.skl.riv.ehr.ehrexchange.getehrextractresponder.v1.GetEhrExtractContinuationRequestType;
+import se.skl.riv.ehr.ehrexchange.getehrextractresponder.v1.GetEhrExtractContinuationResponseType;
+import se.skl.riv.ehr.ehrexchange.getehrextractresponder.v1.GetEhrExtractRequestType;
+import se.skl.riv.ehr.ehrexchange.getehrextractresponder.v1.GetEhrExtractResponseType;
+import se.skl.riv.ehr.ehrexchange.getehrextractresponder.v1.PingRequestType;
+import se.skl.riv.ehr.ehrexchange.getehrextractresponder.v1.PingResponseType;
+import se.skl.riv13606.v2.EHREXTRACT;
+import se.skl.riv13606.v2.II;
+import se.skl.riv13606.v2.TS;
+import se.skl.rivta.bp20.refapp.util.Util;
+
+@WebService(
+	serviceName = "GetEhrExtractResponderService", 
+	endpointInterface="se.skl.riv.ehr.ehrexchange.getehrextract.v1.rivtabpip20.GetEhrExtractResponderInterface", 
+	portName = "GetEhrExtractResponderPort", 
+	targetNamespace = "urn:riv:ehr:ehrexchange:GetEhrExtract:1:rivtabpip20",
+	wsdlLocation = "schemas/interactions/GetEhrExtractInteraction_1.0_rivtabpip20.wsdl")
+public class Responder implements GetEhrExtractResponderInterface {
+
+	static private final Logger logger = LoggerFactory.getLogger(Util.class);
+
+	public GetEhrExtractResponseType getEhrExtract(
+			AttributedURIType logicalAddress,
+			AssertionType hcAssertion,
+			GetEhrExtractRequestType parameters) {
+
+		logger.info("Operation getEhrExtract called");
+		try {
+			GetEhrExtractResponseType response = new GetEhrExtractResponseType();
+
+			EHREXTRACT e = createEhrExtract(parameters);
+
+			response.getEhrExtract().add(e);
+
+			return response;
+
+		} catch (RuntimeException e) {
+			System.out.println("Error occured: " + e);
+			throw e;
+		}
+	}
+
+	public GetEhrExtractContinuationResponseType getEhrExtractContinuation(
+			AttributedURIType logicalAddress,
+			AssertionType hcAssertion,
+			GetEhrExtractContinuationRequestType parameters) {
+
+		logger.info("Operation getEhrExtractContinuation called");
+
+		GetEhrExtractContinuationResponseType response = new GetEhrExtractContinuationResponseType();
+		return response;
+	}
+
+	public PingResponseType ping(AttributedURIType logicalAddress, PingRequestType parameters) {
+		logger.info("Operation ping called");
+		PingResponseType response = new PingResponseType();
+		response.setInfo(parameters.getInfo());
+		response.setLogicalAddress(logicalAddress.getValue());
+		return response;
+	}
+
+	private EHREXTRACT createEhrExtract(GetEhrExtractRequestType parameters) {
+		EHREXTRACT e = new EHREXTRACT();
+
+		II ap = new II();
+		ap.setRoot("AuthorisingParty-UID");
+		e.setAuthorisingParty(ap);
+
+		II eid = new II();
+		eid.setRoot("EhrId-UID");
+		e.setEhrId(eid);
+
+		II es = new II();
+		es.setRoot("EhrSystem-UID");
+		e.setEhrSystem(es);
+
+		e.setRmId("RmId");
+
+		// relay something back that can be verified at the original
+		// requester (end user)
+		II soc = new II();
+		soc.setRoot("SubjectOfCare-UID");
+		soc.setFlavorId("sugar");
+		II socRequest = parameters.getSubjectOfCareId();
+		String identifierName = socRequest.getRoot();
+		soc.setIdentifierName(identifierName);
+		e.setSubjectOfCare(soc);
+
+		TS tc = new TS();
+		tc.setValue(new Date().toString());
+		e.setTimeCreated(tc);
+		return e;
+	}
+
+}
