2de2642415e81c18987bac35d4e0ddaee42ea755
johaneltes <johaneltes@c1faef40-eec5-6430-f181-9cb8a2d1d0ae>
2010-05-20 16:35:25 +0000

    Clarifications and some tydyiing of the bat file

--- ServiceInteractions/riv/crm/scheduling/trunk/wcf/generate-src.bat
+++ ServiceInteractions/riv/crm/scheduling/trunk/wcf/generate-src.bat
@@ -18,4 +18,47 @@
 @REM specific language governing permissions and limitations
 @REM under the License.
 @REM ---------------------------------------------------------------------------------
-svcutil.exe /language:cs /wrapped /out:generated-src\CrmSchedulingInteractions.cs /config:generated-src\app.config /namespace:*,CrmScheduling.Schemas.v1 ..\schemas\interactions\CancelBookingInteraction\CancelBookingInteraction_1.0_rivtabp20.wsdl ..\schemas\interactions\CancelBookingInteraction\*.xsd ..\schemas\interactions\GetAllTimeTypesInteraction\GetAllTimeTypesInteraction_1.0_rivtabp20.wsdl ..\schemas\interactions\GetAllTimeTypesInteraction\*.xsd ..\schemas\interactions\GetAvailableDatesInteraction\GetAvailableDatesInteraction_1.0_rivtabp20.wsdl ..\schemas\interactions\GetAvailableDatesInteraction\*.xsd ..\schemas\interactions\GetAvailableTimeslotsInteraction\GetAvailableTimeslotsInteraction_1.0_rivtabp20.wsdl ..\schemas\interactions\GetAvailableTimeslotsInteraction\*.xsd ..\schemas\interactions\GetBookingDetailsInteraction\GetBookingDetailsInteraction_1.0_rivtabp20.wsdl ..\schemas\interactions\GetBookingDetailsInteraction\*.xsd ..\schemas\interactions\GetCancelledAndRebookedInteraction\GetCancelledAndRebookedInteraction_1.0_rivtabp20.wsdl ..\schemas\interactions\GetCancelledAndRebookedInteraction\*.xsd ..\schemas\interactions\GetSubjectOfCareScheduleInteraction\GetSubjectOfCareScheduleInteraction_1.0_rivtabp20.wsdl ..\schemas\interactions\GetSubjectOfCareScheduleInteraction\*.xsd ..\schemas\interactions\MakeBookingInteraction\MakeBookingInteraction_1.0_rivtabp20.wsdl ..\schemas\interactions\MakeBookingInteraction\*.xsd ..\schemas\interactions\UpdateBookingInteraction\UpdateBookingInteraction_1.0_rivtabp20.wsdl ..\schemas\interactions\UpdateBookingInteraction\*.xsd ..\schemas\core_components\*.xsd
+
+CD ..
+SET OUTFILE=/out:wcf\generated-src\CrmSchedulingInteractions.cs
+SET APPCONFIG=/config:wcf\generated-src\app.config
+SET NAMESPACE=/namespace:*,Riv.Crm.Scheduling.Schemas.v1
+SET SCHEMADIR="schemas\interactions"
+SET SVCUTIL="svcutil.exe"
+
+SET W1=%SCHEMADIR%\CancelBookingInteraction\CancelBookingInteraction_1.0_rivtabp20.wsdl
+SET X1=%SCHEMADIR%\CancelBookingInteraction\*.xsd
+
+SET W2=%SCHEMADIR%\GetAllTimeTypesInteraction\GetAllTimeTypesInteraction_1.0_rivtabp20.wsdl
+SET X2=%SCHEMADIR%\GetAllTimeTypesInteraction\*.xsd
+
+SET W3=%SCHEMADIR%\GetAvailableDatesInteraction\GetAvailableDatesInteraction_1.0_rivtabp20.wsdl
+SET X3=%SCHEMADIR%\GetAvailableDatesInteraction\*.xsd
+
+SET W4=%SCHEMADIR%\GetAvailableTimeslotsInteraction\GetAvailableTimeslotsInteraction_1.0_rivtabp20.wsdl
+SET X4=%SCHEMADIR%\GetAvailableTimeslotsInteraction\*.xsd
+
+SET W5=%SCHEMADIR%\GetBookingDetailsInteraction\GetBookingDetailsInteraction_1.0_rivtabp20.wsdl
+SET X5=%SCHEMADIR%\GetBookingDetailsInteraction\*.xsd
+
+SET W6=%SCHEMADIR%\GetCancelledAndRebookedInteraction\GetCancelledAndRebookedInteraction_1.0_rivtabp20.wsdl
+SET X6=%SCHEMADIR%\GetCancelledAndRebookedInteraction\*.xsd
+
+SET W7=%SCHEMADIR%\GetSubjectOfCareScheduleInteraction\GetSubjectOfCareScheduleInteraction_1.0_rivtabp20.wsdl
+SET X7=%SCHEMADIR%\GetSubjectOfCareScheduleInteraction\*.xsd
+
+SET W8=%SCHEMADIR%\MakeBookingInteraction\MakeBookingInteraction_1.0_rivtabp20.wsdl
+SET X8=%SCHEMADIR%\MakeBookingInteraction\*.xsd
+
+SET W9=%SCHEMADIR%\UpdateBookingInteraction\UpdateBookingInteraction_1.0_rivtabp20.wsdl
+SET X9=%SCHEMADIR%\UpdateBookingInteraction\*.xsd 
+
+SET X10=schemas\core_components\*.xsd
+
+SET SCHEMAS=%W1% %X1% %W2% %X2% %W3% %X3% %W4% %X4% %W5% %X5% %W6% %X6% %W7% %X7% %W8% %X8% %W9% %X9% %X10%
+
+%SVCUTIL% /language:cs /wrapped %OUTFILE% %APPCONFIG% %NAMESPACE% %SCHEMAS%
+
+CD wcf
+
+ECHO I DotNetprojektet ska du ta lagga till referens till System.ServiceModel
