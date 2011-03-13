e3f9d0b9e960c7764d221140e1a450fde884d54d
johaneltes <johaneltes@c1faef40-eec5-6430-f181-9cb8a2d1d0ae>
2011-03-13 00:37:06 +0000

    First cut of revision 1.1

--- ServiceInteractions/riv/crm/scheduling/trunk/wcf/generate-src.bat
+++ ServiceInteractions/riv/crm/scheduling/trunk/wcf/generate-src.bat
@@ -5,7 +5,7 @@
 @REM Licensed to the Apache Software Foundation (ASF) under one
 @REM or more contributor license agreements. See the NOTICE file
 @REM distributed with this work for additional information
-@REM regarding copyright ownership. The ASF licenses this file
+@REM regarding copyright ownership. Inera AB licenses this file
 @REM to you under the Apache License, Version 2.0 (the
 @REM "License"); you may not use this file except in compliance
 @REM with the License. You may obtain a copy of the License at
@@ -26,39 +26,51 @@ SET NAMESPACE=/namespace:*,Riv.Crm.Scheduling.Schemas.v1
 SET SCHEMADIR="schemas\interactions"
 SET SVCUTIL="svcutil.exe"
 
-SET W1=%SCHEMADIR%\CancelBookingInteraction\CancelBookingInteraction_1.0_rivtabp20.wsdl
+SET W1=%SCHEMADIR%\CancelBookingInteraction\CancelBookingInteraction_1.1_rivtabp20.wsdl
 SET X1=%SCHEMADIR%\CancelBookingInteraction\*.xsd
 
-SET W2=%SCHEMADIR%\GetAllTimeTypesInteraction\GetAllTimeTypesInteraction_1.0_rivtabp20.wsdl
+SET W2=%SCHEMADIR%\GetAllTimeTypesInteraction\GetAllTimeTypesInteraction_1.1_rivtabp20.wsdl
 SET X2=%SCHEMADIR%\GetAllTimeTypesInteraction\*.xsd
 
-SET W3=%SCHEMADIR%\GetAvailableDatesInteraction\GetAvailableDatesInteraction_1.0_rivtabp20.wsdl
+SET W3=%SCHEMADIR%\GetAvailableDatesInteraction\GetAvailableDatesInteraction_1.1_rivtabp20.wsdl
 SET X3=%SCHEMADIR%\GetAvailableDatesInteraction\*.xsd
 
-SET W4=%SCHEMADIR%\GetAvailableTimeslotsInteraction\GetAvailableTimeslotsInteraction_1.0_rivtabp20.wsdl
+SET W4=%SCHEMADIR%\GetAvailableTimeslotsInteraction\GetAvailableTimeslotsInteraction_1.1_rivtabp20.wsdl
 SET X4=%SCHEMADIR%\GetAvailableTimeslotsInteraction\*.xsd
 
-SET W5=%SCHEMADIR%\GetBookingDetailsInteraction\GetBookingDetailsInteraction_1.0_rivtabp20.wsdl
+SET W5=%SCHEMADIR%\GetBookingDetailsInteraction\GetBookingDetailsInteraction_1.1_rivtabp20.wsdl
 SET X5=%SCHEMADIR%\GetBookingDetailsInteraction\*.xsd
 
-SET W6=%SCHEMADIR%\GetCancelledAndRebookedInteraction\GetCancelledAndRebookedInteraction_1.0_rivtabp20.wsdl
+SET W6=%SCHEMADIR%\GetCancelledAndRebookedInteraction\GetCancelledAndRebookedInteraction_1.1_rivtabp20.wsdl
 SET X6=%SCHEMADIR%\GetCancelledAndRebookedInteraction\*.xsd
 
-SET W7=%SCHEMADIR%\GetSubjectOfCareScheduleInteraction\GetSubjectOfCareScheduleInteraction_1.0_rivtabp20.wsdl
+SET W7=%SCHEMADIR%\GetSubjectOfCareScheduleInteraction\GetSubjectOfCareScheduleInteraction_1.1_rivtabp20.wsdl
 SET X7=%SCHEMADIR%\GetSubjectOfCareScheduleInteraction\*.xsd
 
-SET W8=%SCHEMADIR%\MakeBookingInteraction\MakeBookingInteraction_1.0_rivtabp20.wsdl
+SET W8=%SCHEMADIR%\MakeBookingInteraction\MakeBookingInteraction_1.1_rivtabp20.wsdl
 SET X8=%SCHEMADIR%\MakeBookingInteraction\*.xsd
 
-SET W9=%SCHEMADIR%\UpdateBookingInteraction\UpdateBookingInteraction_1.0_rivtabp20.wsdl
+SET W9=%SCHEMADIR%\UpdateBookingInteraction\UpdateBookingInteraction_1.1_rivtabp20.wsdl
 SET X9=%SCHEMADIR%\UpdateBookingInteraction\*.xsd 
 
-SET X10=schemas\core_components\*.xsd
+SET W10=%SCHEMADIR%\GetAllCareTypesInteraction\GetAllCareTypesInteraction_1.1_rivtabp20.wsdl
+SET X10=%SCHEMADIR%\GetAllCareTypesInteraction\*.xsd
 
-SET SCHEMAS=%W1% %X1% %W2% %X2% %W3% %X3% %W4% %X4% %W5% %X5% %W6% %X6% %W7% %X7% %W8% %X8% %W9% %X9% %X10%
+SET W11=%SCHEMADIR%\GetAllHealthcareFacilitiesInteraction\GetAllHealthcareFacilitiesInteraction_1.1_rivtabp20.wsdl
+SET X11=%SCHEMADIR%\GetAllHealthcareFacilitiesInteraction\*.xsd
 
-%SVCUTIL% /language:cs /wrapped %OUTFILE% %APPCONFIG% %NAMESPACE% %SCHEMAS%
+SET W12=%SCHEMADIR%\GetAllPerformersInteraction\GetAllPerformersInteraction_1.1_rivtabp20.wsdl
+SET X12=%SCHEMADIR%\GetAllPerformersInteraction\*.xsd
+
+SET X13=schemas\core_components\*.xsd
+
+SET SCHEMAS=%W1% %X1% %W2% %X2% %W3% %X3% %W4% %X4% %W5% %X5% %W6% %X6% %W7% %X7% %W8% %X8% %W9% %X9% %W10% %X10% %W11% %X11% %W12% %X12% %X13%
+
+%SVCUTIL% /language:cs %OUTFILE% %APPCONFIG% %NAMESPACE% %SCHEMAS%
 
 CD wcf
-
+ECHO Generating Service contract .Net Binding interfaces and classes for crm:scheduling Release 1.1
+ECHO The following Service Contracts are new for 1.1: GetAllCareTypes, GetAllHealthcareFacilities, GetAllPerformers
+ECHO The following Service Contracts have been updated with maintained producer backwards compatibility: GetAllTimeTypes, MakeBooking
+ECHO The following Service Contracts have not changed since 1.0: CancelBooking, GetAvailableDates, GetAvailableTimeslots, GetBookingDetails, GetCancelledAndRebooked, GetSubjectOfCareSchedule, UpdateBooking
 ECHO I DotNetprojektet ska du ta lagga till referens till System.ServiceModel
