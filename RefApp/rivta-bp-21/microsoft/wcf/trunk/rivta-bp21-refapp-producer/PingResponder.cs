51f1a8a8a0ed6079e29392f3db3a376367853979
marcuskrantz <marcuskrantz@c1faef40-eec5-6430-f181-9cb8a2d1d0ae>
2011-05-25 09:15:42 +0000

    Expose ping service in .net refapp

---
+++ RefApp/rivta-bp-21/microsoft/wcf/trunk/rivta-bp21-refapp-producer/PingResponder.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using rivta_bp21_refapp_schemas.wsdl;
+
+namespace rivta_bp21_refapp_producer
+{
+    class PingResponder : PingForConfigurationResponderInterface
+    {
+        public PingForConfigurationResponse PingForConfiguration(PingForConfigurationRequest request)
+        {
+            PingForConfigurationResponse response = new PingForConfigurationResponse();
+            ConfigurationType c1 = new ConfigurationType();
+            c1.name = "os";
+            c1.value = "Mac OSX";
+
+            return response;
+        }
+    }
+}
--- RefApp/rivta-bp-21/microsoft/wcf/trunk/rivta-bp21-refapp-schemas/generate-src.bat
+++ RefApp/rivta-bp-21/microsoft/wcf/trunk/rivta-bp21-refapp-schemas/generate-src.bat
@@ -1,16 +1,10 @@
 @REM --------------------------------------------------------------------------
 @REM 
-@REM Generates WCF Proxy and config code for RIV13606 EhrExtraction Interaction
+@REM Generates WCF Proxy and config code for MakeBooking Interaction
 @REM 
 @REM --------------------------------------------------------------------------
 @REM 
 @REM Problem with adressing the wsdl locally. Workaround in place is to use the Java - Apache CXF Web Service to provide the wsdl.
-@REM 
-@REM The command should look like:
-@REM 	svcutil.exe /language:cs /out:EhrExtractionResponderProxy.cs /config:app.config business\JournalinfoApoteketRIV\EhrExtractionInteraction_1.1_rivtabp20.wsdl business\JournalinfoApoteketRIV\*.xsd  technical\wsaddressing\ws-addressing-1.0.xsd
-@REM 
-@REM But gives us errormessages like
-@REM 	Validation Error: Type 'urn:riv13606:v1:II' is not declared.
-@REM 
 @REM --------------------------------------------------------------------------
 svcutil.exe /language:cs /out:generated-src\MakeBookingResponderProxy.cs /config:generated-src\app.config /namespace:urn:riv:crm:scheduling:MakeBooking:1:rivtabp21,rivta_bp21_refapp_schemas.wsdl /namespace:urn:riv:crm:scheduling:MakeBookingResponder:1,rivta_bp21_refapp_schemas.xsd interactions\MakeBookingInteraction\MakeBookingInteraction_1.1_rivtabp21.wsdl interactions\MakeBookingInteraction\*.xsd core_components\*.xsd
+svcutil.exe /language:cs /out:generated-src\PingForConfigurationResponderProxy.cs /config:generated-src\pingapp.config /namespace:urn:riv:itintegration:monitoring:PingForConfiguration:1:rivtabp21,rivta_bp21_refapp_schemas.wsdl /namespace:urn:riv:itintegration:monitoring:PingForConfigurationResponder:1,rivta_bp21_refapp_schemas.xsd interactions\PingForConfiguration\PingForConfigurationInteraction_1.0_rivtabp21.wsdl interactions\PingForConfiguration\*.xsd core_components\*.xsd
