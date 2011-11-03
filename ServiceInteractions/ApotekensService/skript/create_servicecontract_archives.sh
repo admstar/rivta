0f1a69d3aa45d4f5e4efc485ac5c3b51c659f8de
matsek <matsek@c1faef40-eec5-6430-f181-9cb8a2d1d0ae>
2011-11-03 15:31:20 +0000

    first draft

---
+++ ServiceInteractions/ApotekensService/skript/create_servicecontract_archives.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+
+# Creates service contract zip archives from subversion
+#
+# 1. make sure your current working directory is the service contract package
+# 2. run this program
+
+# package name is super directory
+top=$( expr $( pwd ) : '.*/\(.*\)' )
+pkg=${top//_/.}
+
+# get version from user
+read -s -p "Version (ex: 1.0-beta): " ver
+echo ""
+[ -z "$ver" ] && echo "Error: Version must be specified"; exit 1
+
+echo "Runs a subverison update"
+
+# update, and get latest subversion rev
+rev=$( expr "$( svn up )" : 'At revision \(.*\)\.' )
+
+for d in $( echo */); do
+    arch="ServiceContracts_${pkg}_$( basename $d )_${ver}-r${rev}.zip"
+    echo "Processing --> \"$arch\""
+    zip -rq $arch $d -x "*.DS_Store" -x "*.svn/*"
+done
