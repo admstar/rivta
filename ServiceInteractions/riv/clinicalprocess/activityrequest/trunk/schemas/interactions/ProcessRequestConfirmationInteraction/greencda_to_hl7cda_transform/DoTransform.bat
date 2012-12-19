aa8d75c6dfa221c21356d14273835334f0bbd67c
torbjorn.dahlin <torbjorn.dahlin@c1faef40-eec5-6430-f181-9cb8a2d1d0ae>
2012-12-19 16:04:28 +0000

    Tillagda GreenCDA-transformer Uppdateringar

---
+++ ServiceInteractions/riv/clinicalprocess/activityrequest/trunk/schemas/interactions/ProcessRequestConfirmationInteraction/greencda_to_hl7cda_transform/DoTransform.bat
@@ -0,0 +1,4 @@
+@echo off
+
+AltovaXML /xslt2 "MappingMapToCDA.xslt" /in "samples/ProcessRequestConfirmationInteraction_in.xml" /out "../../../../../../../Resources/HL7 CDA/infrastructure/cda/CDA.xml" %*
+IF ERRORLEVEL 1 EXIT/B %ERRORLEVEL%
