fa5732c519d5fcd4a7f1118a6ed2466198ae294b
johan.eltes@callistaenterprise.se <johan.eltes@callistaenterprise.se@c1faef40-eec5-6430-f181-9cb8a2d1d0ae>
2013-06-12 20:52:08 +0000

    regenerated

--- ServiceInteractions/riv/crm/requeststatus/trunk/wcf/generate-src-RIVTABP21.bat
+++ ServiceInteractions/riv/crm/requeststatus/trunk/wcf/generate-src-RIVTABP21.bat
@@ -6,7 +6,7 @@
 	@REM Licensed to the Apache Software Foundation (ASF) under one
 	@REM or more contributor license agreements. See the NOTICE file
 	@REM distributed with this work for additional information
-	@REM regarding copyright ownership. Inera AB licenses this file
+	@REM regarding copyright ownership. Stockholm County Council licenses this file
 	@REM to you under the Apache License, Version 2.0 (the
 	@REM "License"); you may not use this file except in compliance
 	@REM with the License. You may obtain a copy of the License at
@@ -23,15 +23,12 @@
 	
 	SET SCHEMADIR=schemas
 	
-	SET W0=%SCHEMADIR%\.\interactions\GetRequestActivitiesInteraction\GetAggregatedRequestActivitiesInteraction_1.0_RIVTABP21.wsdl
+	SET W0=%SCHEMADIR%\.\interactions\GetRequestActivitiesInteraction\GetRequestActivitiesInteraction_1.0_RIVTABP21.wsdl
 SET X0=%SCHEMADIR%\interactions\GetRequestActivitiesInteraction\*.xsd
-
-SET W1=%SCHEMADIR%\.\interactions\GetRequestActivitiesInteraction\GetRequestActivitiesInteraction_1.0_RIVTABP21.wsdl
-SET X1=%SCHEMADIR%\interactions\GetRequestActivitiesInteraction\*.xsd
 
 SET XCORE=%SCHEMADIR%\core_components\*.xsd
 
-SET SCHEMAS=%XCORE% %W0% %X0% %W1% %X1% 
+SET SCHEMAS=%XCORE% %W0% %X0% 
 
 SET OUTFILE=/out:wcf\generated-src\CrmRequeststatusInteractions.cs
 SET APPCONFIG=/config:wcf\generated-src\app.config
