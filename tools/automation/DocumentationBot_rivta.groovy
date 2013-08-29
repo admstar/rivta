33f86b9e72772f731c1a750b6a28b46f459dcfd1
peter.hernfalk@gmail.com <peter.hernfalk@gmail.com@c1faef40-eec5-6430-f181-9cb8a2d1d0ae>
2013-08-29 09:02:09 +0000

    Added generation of the Service Domain table that is published on the RIVTA site

--- tools/automation/DocumentationBot_rivta.groovy
+++ tools/automation/DocumentationBot_rivta.groovy
@@ -20,6 +20,7 @@
 import static groovy.io.FileType.FILES
 
 //-----Definitions
+cehisCategoriesLink = "http://code.google.com/p/rivta/wiki/CehisKategorier"
 excelMasterFile = []
 freeplaneMapBeginning = []
 freeplaneMapBeginning[0]  = '<map version="freeplane 1.2.0">'
@@ -78,31 +79,42 @@ freeplaneMapBeginning[52] = '<hook NAME="AutomaticEdgeColor" COUNTER="2"/>'
 freeplaneMapEnd = []
 freeplaneMapEnd[0] = '</node>'
 freeplaneMapEnd[1] = '</map>'
-HtmlTableBeginning = []
-HtmlTableBeginning[0]  = '<table border="1" cellpadding="1" cellspacing="1" style="width: 730px; height: 1396px;">'
-HtmlTableBeginning[1]  = '<tbody>'
-HtmlTableBeginning[2]  = '<tr>'
-HtmlTableBeginning[3]  = '<td>'
-HtmlTableBeginning[4]  = '<p><strong>Svenskt tjänstedomännamn</strong></p>'
-HtmlTableBeginning[5]  = '</td>'
-HtmlTableBeginning[6]  = '<td>'
-HtmlTableBeginning[7]  = '<p>'
-HtmlTableBeginning[8]  = '<strong><strong><br />'
-HtmlTableBeginning[9]  = 'Namn på google code, rivta-webben</strong></strong></p>'
-HtmlTableBeginning[10] = '</td>'
-HtmlTableBeginning[11] = '<td>'
-HtmlTableBeginning[12] = '<p><strong>Ingående tjänstekontrakt</strong></p>'
-HtmlTableBeginning[13] = '</td>'
-HtmlTableBeginning[14] = '<td>'
-HtmlTableBeginning[15] = '<p><strong>Tjänstedomän-förvaltare, kontakt</strong></p>'
-HtmlTableBeginning[16] = '</td>'
-HtmlTableBeginning[17] = '</tr>'
-HtmlTableEnd = []
-HtmlTableEnd[0] = '</tbody>'
-HtmlTableEnd[1] = '</table>'
-htmltTableFile = "/_devHtmlTables.html"
-htbSize = HtmlTableBeginning.size
-hteSize = HtmlTableEnd.size
+htmlTableFileRIVTA = "/_HtmlTableRIVTA.html"
+htmlTableFileVifo = "/_devHtmlTablesVifo.html"
+HtmlTableRIVTABeginning = []
+HtmlTableRIVTABeginning[0]  = '<p>'
+HtmlTableRIVTABeginning[1]  = '<table class="wikitable">'
+HtmlTableRIVTABeginning[2]  = '<tr>'
+HtmlTableRIVTAEnd = []
+HtmlTableRIVTAEnd[0] = '</tbody>'
+HtmlTableRIVTAEnd[1] = '</table>'
+htbSizeRIVTA = HtmlTableRIVTABeginning.size
+hteSizeRIVTA = HtmlTableRIVTAEnd.size
+
+HtmlTableVifoBeginning = []
+
+HtmlTableVifoBeginning[0]  = '<table border="1" cellpadding="1" cellspacing="1" style="width: 730px; height: 1396px;">'
+HtmlTableVifoBeginning[1]  = '<tbody>'
+HtmlTableVifoBeginning[2]  = '<tr>'
+HtmlTableVifoBeginning[3]  = '<td>'
+HtmlTableVifoBeginning[4]  = '<p><strong>Svenskt tjänstedomännamn</strong></p>'
+HtmlTableVifoBeginning[5]  = '</td>'
+HtmlTableVifoBeginning[6]  = '<td>'
+HtmlTableVifoBeginning[7]  = '<p>'
+HtmlTableVifoBeginning[8]  = '<strong><strong><br />'
+HtmlTableVifoBeginning[9]  = 'Namn på google code, rivta-webben</strong></strong></p>'
+HtmlTableVifoBeginning[10] = '</td>'
+HtmlTableVifoBeginning[11] = '<td>'
+HtmlTableVifoBeginning[12] = '<p><strong>Ingående tjänstekontrakt</strong></p>'
+HtmlTableVifoBeginning[13] = '</td>'
+HtmlTableVifoBeginning[14] = '<td>'
+HtmlTableVifoBeginning[15] = '<p><strong>Tjänstedomän-förvaltare, kontakt</strong></p>'
+HtmlTableVifoBeginning[16] = '</td>'
+HtmlTableVifoEnd = []
+HtmlTableVifoEnd[0] = '</table>'
+HtmlTableVifoEnd[1] = '</p>'
+htbSizeVifo = HtmlTableVifoBeginning.size
+hteSizeVifo = HtmlTableVifoEnd.size
 
 logFile = "/_logFile.txt"
 loglevelDebug = "DEBUG"
@@ -123,7 +135,7 @@ outputVIFOStructure = "VIFO"
 codeStructureLevel1 = "Published Service contracts"
 output2All = "all"
 output2Mindmap = "mindmap"
-output2HtmlTable = "htmlTable"
+output2HtmlTableVifo = "htmlTable"
 publishStatus4Wsdl = ['GetAllHealthcareFacilitiesInteraction_1.1_RIVTABP21.wsdl':true, 'DiscontinuePrescriptionInteraction_1.0_RIVTABP21.wsdl':true]
 RIVTADomainFolder = "http://rivta.googlecode.com/svn/ServiceInteractions/riv/"
 RIVTACheckoutCommand = "svn checkout " + RIVTADomainFolder + " "
@@ -135,10 +147,11 @@ wsdlList = []
 charset = "ISO-8859-1"                        //--- ("ISO-8859-1", "UTF-8")
 delimiterToken = '/'                         //--- (mac = '/', windows = '.')
 downloadTKFiles = false                      //--- (true, false)
-excelFile = "/Users/peterhernfalk/Desktop/_Peter_Files/Ineraprojekt/AL/Aktiviteter/Landskap med TP och TK/Groovyscript/Underlag/MasterTest.xls"
+excelFile = "/Users/peterhernfalk/Desktop/_Peter_Files/HOS-projekt/AL/Aktiviteter/Landskap med TP och TK/Groovyscript/Underlag/MasterTest.xls"
+//excelFile = "/Users/peterhernfalk/Desktop/_Peter_Files/HOS-projekt/AL/Aktiviteter/Landskap med TP och TK/Groovyscript/Underlag/MasterTest_0.4.xls"
 mindmapStructure = outputCodeStructure       //--- (outputCodeStructure, outputVIFOStructure)
-outputType = output2All                  //--- (output2All, output2Mindmap,output2HtmlTable) 
-RIVTATargetFolder = "/Users/peterhernfalk/Desktop/_Peter_Files/rivta/"
+outputType = output2All                  //--- (output2All, output2Mindmap,output2HtmlTableRIVTA,output2HtmlTableVifo)
+localRIVTATargetFolder = "/Users/peterhernfalk/Desktop/_Peter_Files/rivta/"
 useAndVerifyDocumentationURL = false           //--- (true, false)
 useStatusFilter = false                      //--- (true, false)
 useVIFOLevel1ForCodeStructure = false         //--- (true, false)
@@ -146,11 +159,11 @@ useVIFOLevel1ForCodeStructure = false         //--- (true, false)
 
 
 //-----Downloads all TK files from the RIV-TA site
-RIVTACheckoutCommandToFolder = RIVTACheckoutCommand + RIVTATargetFolder
+RIVTACheckoutCommandToFolder = RIVTACheckoutCommand + localRIVTATargetFolder
 if (downloadTKFiles == true) {
 
     //-----Delete the local target folder before download
-    new File(RIVTATargetFolder).deleteDir()
+    new File(localRIVTATargetFolder).deleteDir()
     
     //-----Download files from the RIV-TA site to the local target folder
     def process = RIVTACheckoutCommandToFolder.execute()
@@ -163,15 +176,15 @@ if (useStatusFilter == true) {
 }  
 
 //-----Analyze and filter the downloaded TK files, extract information ad write it to a temporary map
-new File(RIVTATargetFolder).eachFileRecurse(FILES) {
+new File(localRIVTATargetFolder).eachFileRecurse(FILES) {
     if((it.name.endsWith('.wsdl')) && (it.path.contains("trunk"))) {
         
         ext = it.path.tokenize(delimiterToken)
 
         //-----VIFO-structure: look up and use the swedish names that corresponds to the path tokens
-        def pathLevel2 = ext[RIVTATargetFolder.count(delimiterToken)-1]
-        def pathLevel3 = ext[RIVTATargetFolder.count(delimiterToken)]
-        def pathLevel4 = ext[RIVTATargetFolder.count(delimiterToken)+1]
+        def pathLevel2 = ext[localRIVTATargetFolder.count(delimiterToken)-1]
+        def pathLevel3 = ext[localRIVTATargetFolder.count(delimiterToken)]
+        def pathLevel4 = ext[localRIVTATargetFolder.count(delimiterToken)+1]
 
 
         //-----Chosen language
@@ -193,7 +206,7 @@ new File(RIVTATargetFolder).eachFileRecurse(FILES) {
         }
         
         //-----Initialize the Level variables
-        if (Level1 == null) { Level1 = ext[RIVTATargetFolder.count(delimiterToken)-2] }
+        if (Level1 == null) { Level1 = ext[localRIVTATargetFolder.count(delimiterToken)-2] }
         if (Level2 == null) { Level2 = pathLevel2 }
         if (Level3 == null) { Level3 = pathLevel3 }
         if (Level4 == null) { Level4 = pathLevel4 }
@@ -208,7 +221,7 @@ new File(RIVTATargetFolder).eachFileRecurse(FILES) {
             def wsdl = xml.children().children().each { 
                 if (it.name() == "operation") { 
                     def tkPrefix = ""        //"TK: "
-                    /*if (outputType == output2HtmlTable) {
+                    /*if (outputType == output2HtmlTableVifo) {
                         tkPrefix = ""
                     }*/
                     Level6 = tkPrefix + it.attributes().name
@@ -235,7 +248,7 @@ if ((outputType == output2Mindmap) || (outputType == output2All)) {
     ///////////////////////////////////////////////////////////////////////////////////
 */
     //-----Write a Freeplane file, containing all Service Contract descriptions
-    new File(RIVTATargetFolder + mindmapFile).newOutputStream().withWriter(charset) { 
+    new File(localRIVTATargetFolder + mindmapFile).newOutputStream().withWriter(charset) { 
         writer -> mbSize.times { writer.write freeplaneMapBeginning[it] + "\n" }
     }
     
@@ -246,7 +259,7 @@ if ((outputType == output2Mindmap) || (outputType == output2All)) {
     def lastLevel4 = ""
     def firstTime = true
     
-    new File(RIVTATargetFolder + mindmapFile).withWriterAppend(charset) { 
+    new File(localRIVTATargetFolder + mindmapFile).withWriterAppend(charset) { 
         writer -> wsdlListSize.times { 
             def ext = wsdlList[it].tokenize(delimiterToken)
             //-----2do: look up and use the swedish names that corresponds to the path tokens
@@ -337,38 +350,52 @@ if ((outputType == output2Mindmap) || (outputType == output2All)) {
     }
         
     //-----Write the last part of the mindmap file
-    new File(RIVTATargetFolder + mindmapFile).withWriterAppend(charset) { 
+    new File(localRIVTATargetFolder + mindmapFile).withWriterAppend(charset) { 
         writer -> meSize.times { writer.write freeplaneMapEnd[it] + "\n" }
     }
-} 
+}
 
-if ((outputType == output2HtmlTable) || (outputType == output2All)) {
 
-    /** 
+//-----Fetch a copy of the contents of the Excel file. Store the data in a map that contains named attributes
+def index = 0
+def excel = new ExcelReader(excelFile)
+
+excel.eachLine {
+    excelMasterFile[index] = [
+            contract:"${cell(0)}".trim(),
+            contractcategory:"${cell(1)}".trim(),
+            swedishsubdomain:"${cell(2)}".trim(),
+            englishsubdomain:"${cell(3)}".trim(),
+            vifodomainswedish:"${cell(4)}".trim(),
+            vifodomainenglish:"${cell(5)}".trim(),
+            rivtaname:"${cell(6)}".trim(),
+            rivtaservicedomain:"${cell(7)}".trim(),
+            domaincategory:"${cell(8)}".trim(),
+            domaincontact:"${cell(9)}".trim(),
+            version:"${cell(10)}".trim(),
+            rivtabp20:"${cell(11)}".trim(),
+            rivtabp21:"${cell(12)}".trim()
+    ]
+    log(loglevelDebug, "excelMasterFile[index]: " + excelMasterFile[index])
+    index += 1
+}
+
+
+if ((outputType == output2HtmlTableVifo) || (outputType == output2All)) {
+
+    /**
     ///////////////////////////////////////////////////////////////////////////////////
     ///////////////////////////////////////////////////////////////////////////////////
-    ///////////////////////////// Write html file /////////////////////////////////////
+    ///////////////////////////// Write html file (Vifo) //////////////////////////////
     ///////////////////////////////////////////////////////////////////////////////////
     ///////////////////////////////////////////////////////////////////////////////////
     */
 
-    //-----Fetch a copy of the contents of the Excel file. Store the data in a map that contains named attributes
-    def index = 0
-    new ExcelReader(excelFile).eachLine {
-        excelMasterFile[index] = [
-                contract:"${cell(0)}".trim(), contractcategory:"${cell(1)}".trim(),
-                rivtaname:"${cell(2)}".trim(), domaincategory:"${cell(3)}".trim(),
-                vifodomain:"${cell(4)}".trim(), swedishdomain:"${cell(5)}".trim(),
-                domaincontact:"${cell(6)}".trim()]
-        log(loglevelDebug, "excelMasterFile[index]: " + excelMasterFile[index])
-        index += 1
-    }
-
     //-----2do: recursive logic that creates as many tables as needed
     
     //-----Write a Html Table file, containing all Service Contract descriptions
-    new File(RIVTATargetFolder + htmltTableFile).newOutputStream().withWriter(charset) { 
-        writer -> htbSize.times { writer.write HtmlTableBeginning[it] + "\n" }
+    new File(localRIVTATargetFolder + htmlTableFileVifo).newOutputStream().withWriter(charset) {
+        writer -> htbSizeVifo.times { writer.write HtmlTableVifoBeginning[it] + "\n" }
     }
 
     //-----Write nodes in table format to the html file
@@ -380,7 +407,7 @@ if ((outputType == output2HtmlTable) || (outputType == output2All)) {
     def lastServiceDomainName = ""
     def firstTime = true
 
-    new File(RIVTATargetFolder + htmltTableFile).withWriterAppend(charset) { 
+    new File(localRIVTATargetFolder + htmlTableFileVifo).withWriterAppend(charset) {
         writer -> wsdlListSize.times {
             def ext = wsdlList[it].tokenize(delimiterToken)
             def newLevel1 = ext[0]
@@ -393,7 +420,7 @@ if ((outputType == output2HtmlTable) || (outputType == output2All)) {
             def urlAllLevels = RIVTADomainFolder + newLevel2 + delimiterToken + newLevel3 + delimiterToken + newLevel4 + trunkDocsSubPath
 
             //-----Fetch serviceDomainName and serviceDomainContact from the map, containing Excel data
-            serviceDomainName = excelMasterFile.find { it.contract.toLowerCase() == newLevel6.toLowerCase() }?.with { map -> "$map.swedishdomain" }
+            serviceDomainName = excelMasterFile.find { it.contract.toLowerCase() == newLevel6.toLowerCase() }?.with { map -> "$map.swedishsubdomain" }
             if (serviceDomainName == null) { serviceDomainName = " " }
 
             //-----Fetch the Domain contact only when the RIV-Ta name changes
@@ -407,8 +434,8 @@ if ((outputType == output2HtmlTable) || (outputType == output2All)) {
                 firstTime = false
                 lastServiceDomainContact = serviceDomainContact
                 writer.write '<tr>' + "\n"
-                writer.write '<td><p>' + serviceDomainName + '</p></td>' + "\n" 
-                writer.write '<td><p>' + newLevel2 + ":" + newLevel3 + '</p></td>' + "\n" 
+                writer.write tdStringVifo(serviceDomainName) + "\n"
+                writer.write tdStringVifo(newLevel2 + ":" + newLevel3) + "\n"
                 
                 //-----Add a link to the docs folder on the RIV-TA site
                 writer.write '<td>' + "\n"
@@ -433,8 +460,8 @@ if ((outputType == output2HtmlTable) || (outputType == output2All)) {
                     writer.write '</td>' + "\n"
                     writer.write '</tr>' + "\n"
                     writer.write '<tr>' + "\n"
-                    writer.write '<td><p>' + serviceDomainName + '</p></td>' + "\n"
-                    writer.write '<td><p>' + newLevel2 + ":" + newLevel3 + '</p></td>' + "\n"
+                    writer.write tdStringVifo(serviceDomainName) + "\n"
+                    writer.write tdStringVifo(newLevel2 + ":" + newLevel3) + "\n"
                     writer.write '<td>' + "\n" 
                     if (newLevel4 == " ") {
                         if ((useAndVerifyDocumentationURL == true) && (getResponseCode(urlEmptyLevel4) == 200)) {
@@ -460,14 +487,84 @@ if ((outputType == output2HtmlTable) || (outputType == output2All)) {
     }
 
     //-----Write the last part of the Html file
-    new File(RIVTATargetFolder + htmltTableFile).withWriterAppend(charset) { 
-        writer -> hteSize.times { writer.write HtmlTableEnd[it] + "\n" }
+    new File(localRIVTATargetFolder + htmlTableFileVifo).withWriterAppend(charset) {
+        writer -> hteSizeVifo.times { writer.write HtmlTableVifoEnd[it] + "\n" }
     }
+
+}
+
+if ((outputType == output2HtmlTableVifo) || (outputType == output2All)) {
+
+    /**
+     ///////////////////////////////////////////////////////////////////////////////////
+     ///////////////////////////////////////////////////////////////////////////////////
+     ///////////////////////////// Write html file RIVTA //////////////////////////////
+     ///////////////////////////////////////////////////////////////////////////////////
+     ///////////////////////////////////////////////////////////////////////////////////
+     */
+    //-----Write a Html Table file, containing all Service Contract descriptions
+    new File(localRIVTATargetFolder + htmlTableFileRIVTA).newOutputStream().withWriter(charset) {
+        writer -> htbSizeRIVTA.times { writer.write HtmlTableRIVTABeginning[it] + "\n" }
+        writer.write tdStringRIVTA("<strong>Populärnamn</strong>") + "\n"
+        writer.write tdStringRIVTA("<strong>Tjänstedomän</strong>") + "\n"
+        writer.write tdStringRIVTA("<strong>Version</strong>") + "\n"
+        writer.write tdStringRIVTA("<strong>RIV-TA BP 2.0</strong>") + "\n"
+        writer.write tdStringRIVTA("<strong>RIV-TA BP 2.1</strong>") + "\n"
+        writer.write tdStringRIVTA("<a href=" + cehisCategoriesLink + ">Cehis kategori</a>") + "\n"
+        writer.write tdStringRIVTA(" ") + "\n"
+    }
+
+    def lastLevel2 = ""
+    def lastLevel3 = ""
+
+    //-----Write a Html Table file, containing all Service Contract descriptions
+    new File(localRIVTATargetFolder + htmlTableFileRIVTA).withWriterAppend(charset) {
+        writer -> wsdlListSize.times {
+            def ext = wsdlList[it].tokenize(delimiterToken)
+            def newLevel2 = ext[1]
+            def newLevel3 = ext[2]
+            def newLevel6 = ext[5]
+
+            userfriendlyServiceDomainName = excelMasterFile.find { it.contract.toLowerCase() == newLevel6.toLowerCase() }?.with { map -> "$map.swedishsubdomain" }
+            if (userfriendlyServiceDomainName == null) { userfriendlyServiceDomainName = " " }
+            serviceDomainName = excelMasterFile.find { it.contract.toLowerCase() == newLevel6.toLowerCase() }?.with { map -> "$map.rivtaservicedomain" }.toString()
+            version = excelMasterFile.find { it.contract.toLowerCase() == newLevel6.toLowerCase() }?.with { map -> "$map.version" }
+            rivtaBP20 = excelMasterFile.find { it.contract.toLowerCase() == newLevel6.toLowerCase() }?.with { map -> "$map.rivtabp20" }
+            rivtaBP21 = excelMasterFile.find { it.contract.toLowerCase() == newLevel6.toLowerCase() }?.with { map -> "$map.rivtabp21" }
+            serviceContractCategory = excelMasterFile.find { it.contract.toLowerCase() == newLevel6.toLowerCase() }?.with { map -> "$map.contractcategory" }
+            downloadLink = "Saknas i Excel"
+
+            //-----Write HTML table, using available data
+            if ((newLevel2 != lastLevel2) || (newLevel3 != lastLevel3)) {
+                if (userfriendlyServiceDomainName.trim().length() > 0 ) {
+                    writer.write "<tr>" + "\n"
+                    writer.write tdStringRIVTA(userfriendlyServiceDomainName) + "\n"
+                    writer.write tdStringRIVTA(serviceDomainName) + "\n"
+                    writer.write tdStringRIVTA(version) + "\n"
+                    writer.write tdStringRIVTA(rivtaBP20) + "\n"
+                    writer.write tdStringRIVTA(rivtaBP21) + "\n"
+                    writer.write tdStringRIVTA(serviceContractCategory) + "\n"
+                    writer.write tdStringRIVTA(downloadLink) + "\n"
+                    writer.write "</tr>" + "\n"
+
+                }
+            }
+
+            lastLevel2 = newLevel2
+            lastLevel3 = newLevel3
+        }
+    }
+
+            //-----Write the last part of the Html file
+    new File(localRIVTATargetFolder + htmlTableFileRIVTA).withWriterAppend(charset) {
+        writer -> hteSizeRIVTA.times { writer.write HtmlTableRIVTAEnd[it] + "\n" }
+    }
+
 }
 
 
 //-----Write a text file, containing the paths and names of the wsdl files
-File lstFile = new File(RIVTATargetFolder + logFile)
+File lstFile = new File(localRIVTATargetFolder + logFile)
 lstFile.withWriter{ 
     out ->  wsdlList.each {
         out.println it
@@ -479,6 +576,14 @@ ShowExecutionStatistics()
 
 
 //-------------------- Methods --------------------//
+
+def tdStringVifo (String stringContent) {
+    "<td><p>" + stringContent + "</p></td>"
+}
+
+def tdStringRIVTA (String stringContent) {
+    "<td style=\"border: 1px solid #ccc; padding: 5px;\">" + stringContent + "</td>"
+}
 
 /** Adds "mailto:" to a valid e-mail address */
 def addMailto(lastServiceDomainContact) {
