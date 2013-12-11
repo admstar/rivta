d8143e9c53237f6b7321b8e810a1c841a062089b
peter.hernfalk@gmail.com <peter.hernfalk@gmail.com@c1faef40-eec5-6430-f181-9cb8a2d1d0ae>
2013-12-11 10:27:23 +0000

    Minor changes

--- tools/automation/CreateServiceDomainArchive.groovy
+++ tools/automation/CreateServiceDomainArchive.groovy
@@ -1,3 +1,4 @@
+
 /**
  * /////////////////////////////////////////////////////////////////////////////
  * //                                                                         //
@@ -10,6 +11,16 @@
  * Last update: 2013-12-11
  *
  * Script status: one thing remains: add tag name to the archive file. After that the script will be ready for acceptance test
+ *
+ *
+ * Namnsättning av Zip-fil
+ * Zip-filen ska alltid ges samma version som den taggning den motsvarar i Subversion. När en Zipfil
+ * skapas utifrån taggen i Subversion ska prefixet ”ServiceContracts” adderas namnet:
+ * Release:
+ * ServiceContracts_<tjänstedomän>_<major_version>.<minor_version>.zip
+ * Ex. ServiceContracts_clinicalprocess_activityprescription_actoutcome_2.1.zip
+ * Release candidate:
+ * ServiceContracts_<tjänstedomän>_<major_version>.<minor_version>_RC<RC-nummer>.
  */
 
 import java.util.zip.ZipOutputStream
@@ -43,9 +54,12 @@ def getValuesFromParameters() {
 
     def cli = new CliBuilder(usage: mecall, header: 'Options:', footer: medesc)
     cli.ad(longOpt: 'archivedir', args:1, required:true, argName:'Target directory', 'Directory in which the archive file will be stored.')
-    cli.af(longOpt: 'archivefile', args:1, required:true, argName:'Archive file', 'The path and file name of the archive file (.zip) that will be uploaded to the rivta site.')
+    cli.af(longOpt: 'archivefile', args:1, required:true, argName:'Archive file', 'The file name of the archive file (.zip) that will be uploaded to the rivta site.')
     cli.l(longOpt: 'useoptionallogging', args:1, required:false, argName:'Optional: true', 'If "true" then the script logs extra output to the console')
     cli.sd(longOpt: 'sourcedir', args:1, required:true, argName:'Source directory', 'Directory in which the service domain files exists.')
+
+    //-----Will probably need more parameters so that a correct file name can be used for the archive file
+    //cli.??(longOpt: 'tagname??', args:1, required:true, argName:'Tag name', 'The tag name, which is a part of the archive file name.')
 
     //-----Verify all parameters
     def options = cli.parse(args)
