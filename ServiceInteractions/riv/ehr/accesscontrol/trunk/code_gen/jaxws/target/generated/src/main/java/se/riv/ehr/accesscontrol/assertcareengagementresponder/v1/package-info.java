68eee4c6de4133368c0520893f089609aa66a1bf
johan@eltesconsulting.se <johan@eltesconsulting.se@c1faef40-eec5-6430-f181-9cb8a2d1d0ae>
2014-05-03 22:22:24 +0000

    RIVTA BP 21

--- ServiceInteractions/riv/ehr/accesscontrol/trunk/code_gen/jaxws/target/generated/src/main/java/se/riv/ehr/accesscontrol/assertcareengagementresponder/v1/package-info.java
+++
@@ -1,2 +0,0 @@
-@javax.xml.bind.annotation.XmlSchema(namespace = "urn:riv:ehr:accesscontrol:AssertCareEngagementResponder:1", elementFormDefault = javax.xml.bind.annotation.XmlNsForm.QUALIFIED)
-package se.riv.ehr.accesscontrol.assertcareengagementresponder.v1;
