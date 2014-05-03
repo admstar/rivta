4886ed12f3d8ae937b069299f479805432958317
johan@eltesconsulting.se <johan@eltesconsulting.se@c1faef40-eec5-6430-f181-9cb8a2d1d0ae>
2014-05-03 22:20:39 +0000

    Added a bit too much

---
+++ ServiceInteractions/riv/ehr/accesscontrol/trunk/code_gen/jaxws/target/generated/src/main/java/se/riv/ehr/accesscontrol/assertcareengagementresponder/v1/package-info.java
@@ -0,0 +1,2 @@
+@javax.xml.bind.annotation.XmlSchema(namespace = "urn:riv:ehr:accesscontrol:AssertCareEngagementResponder:1", elementFormDefault = javax.xml.bind.annotation.XmlNsForm.QUALIFIED)
+package se.riv.ehr.accesscontrol.assertcareengagementresponder.v1;
