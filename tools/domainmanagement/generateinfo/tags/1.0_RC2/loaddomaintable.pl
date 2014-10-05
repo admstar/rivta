eb03493a3b05f6cdc2beb687c94bd31e3f46c7b6
lars.erik.rojeras@skoview.se <lars.erik.rojeras@skoview.se@c1faef40-eec5-6430-f181-9cb8a2d1d0ae>
2014-10-05 08:54:54 +0000

    New release based on more exact mgmt of TAK information and producers

---
+++ tools/domainmanagement/generateinfo/tags/1.0_RC2/loaddomaintable.pl
@@ -0,0 +1,194 @@
+:- module(loaddomaintable, [
+	      dt_get_domain_acceptance/3 ,
+	      dt_get_domain_table/2 ,
+	      dt_get_zip_link/2 ,
+	      dt_get_zip_link/3 ,
+	      dt_load_domain_table/1 ,
+	      dt_verify/1
+       ]) .
+
+
+:- use_module(leolib).
+
+/* - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
+This exported predicate load information in the recoreded db about all
+domains in Servce Domain Table
+- - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - */
+dt_load_domain_table(URL) :-
+	l_load_dom_http(URL, DOM) ,
+	! ,
+	l_erase_all(domainTable),
+	process_dom(DOM) .
+
+dt_load_domain_table(_).
+
+
+/* - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
+Retrieve the link to the zip archiv
+- - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - */
+dt_get_zip_link(Domain, ZipLink) :-
+	dt_get_zip_link(Domain, _Version, ZipLink) .
+
+dt_get_zip_link(Domain, Version, ZipLink) :-
+	recorded(domainTable, domainTable(
+				 Domain,
+				 _PopularName,
+				 Version,
+				 check(s,_S_code, _S_link),
+				 check(i,_I_code, _I_link),
+				 check(t,_T_code, _T_link),
+				 ZipLink )) .
+
+/* - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
+This is the exported predicate to fetch a row from the domain table
+store
+- - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - */
+dt_get_domain_table(Domain, Version) :-
+	recorded(domainTable, domainTable(
+				 Domain,
+				 _PopularName,
+				 Version,
+				 check(s,_S_code, _S_link),
+				 check(i,_I_code, _I_link),
+				 check(t,_T_code, _T_link),
+				 _ZipLink )) .
+
+
+
+/* - - - - - - - - sv- - - - - - - - - - - - - - - - - - - - - - - - - - - -
+Get the Swedish name as specified on the SD table
+- - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - */
+
+/*
+dt_get_popular_name(Domain, Popular) :-
+	recorded(_, domainTable(Domain, Popular, _,_,_,_,_ )) ,
+	! .
+
+dt_get_popular_name(_, ' ') .
+*/
+
+/* - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
+Get the acceptance level of a version of a domain from the DT-table
+OkType is one of:
+    0 - Godkänd under nuvarande regelverk
+    1 - Godkänd under tidigare regelverk
+    2 - Ej granskad/godkänd
+- - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - */
+dt_get_domain_acceptance(Domain, Version, OkType) :-
+	recorded(_,domainTable(
+		       Domain,
+		       _,
+		       Version,
+		       _,
+		       _,
+		       check(t, 'G', Link),
+		       _ )) ,
+	get_domain_acceptance2(Link, OkType) .
+
+get_domain_acceptance2(Link, 0) :-
+	atom_length(Link, Len), Len > 3 ,  % To be current it must have a link to the review file
+	! .
+get_domain_acceptance2(_Link, 1) .
+
+/* - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
+Verify the loading of the domain table
+- - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - */
+
+dt_verify(No) :-
+	setof(struct(A,B),
+	      dt_get_domain_table(A,B),
+	      List) ,
+	length(List, No) .
+
+
+/* - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
+Load Swedish names from List of CSV lines
+- - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - */
+load_swedish_names([]) :- ! .
+load_swedish_names([Line | Rest]) :-
+	atomic_list_concat([ _, _ , SwName, _, _, _, DomainName | _ ], ';' , Line ) ,
+	atomic_list_concat(Domain, ':', DomainName),
+	! ,
+	recordz(swTable, swName(Domain, SwName)) ,
+	load_swedish_names(Rest) .
+
+% Error is usually due to crlf in fields. Search for '"'
+load_swedish_names([Line | Rest]) :-
+	l_write_trace(['** Error reading Swedish name CSV file, line: ', Line], 0 ) ,
+	load_swedish_names(Rest) .
+
+
+/* - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
+Parse the dom to extract information from the domain table
+- - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - */
+process_dom([]) :- ! .
+
+process_dom([element(tr,_,
+		     [ element(td,_, [DomainName]) ,
+		       element(td,_, [PopularName]) ,
+		       element(td,_, [Version]) ,
+		       element(td,_, _) ,
+		       element(td,_, _) ,
+		       element(td,_, S) ,
+		       element(td,_, I) ,
+		       element(td,_, T) ,
+		       element(td,_, Link)
+		     ]) |Tail]) :-
+	l_strip_blanks(DomainName, DomainName2) ,
+	atom_length(DomainName2, Len),
+	Len > 2 ,
+	! ,
+	atomic_list_concat(DomainNameList, ':', DomainName2) ,
+	l_strip_blanks(PopularName, PopularName2) ,
+	l_strip_blanks(Version, Version2) ,
+	% mk_tag_name(DomainNameList, Version2, TagName) ,
+	% !!! The line above should probably change.
+	%It is really better to save the Version and not try to
+	%guess the tag name at this point. The responsibility of the
+	%matching should be kept in main.pl.
+	l_write_trace(['process_dom 1: Version:', Version2], 2),
+	extract_link_text(S, S_code, S_link) ,
+	extract_link_text(I, I_code, I_link) ,
+	extract_link_text(T, T_code, T_link) ,
+	extract_link_text(Link, _, ZipLink) ,
+	recordz(domainTable, domainTable(
+				 DomainNameList,
+				 PopularName2,
+				 Version2,
+				 check(s,S_code, S_link),
+				 check(i,I_code, I_link),
+				 check(t,T_code, T_link),
+				 ZipLink )) ,
+	l_write_trace(['Domain table - Loaded: ', DomainName2], 2 ),
+	process_dom(Tail) .
+
+process_dom([element(_,_,C)|T]) :-
+	! ,
+	process_dom(C) ,
+	process_dom(T) .
+
+process_dom([H|T]) :-
+	! ,
+	process_dom(H) ,
+	process_dom(T) .
+
+process_dom(_) .
+
+
+/* - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
+Extract link and text
+- - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - */
+extract_link_text(List, Text2, Link) :-
+	member(element(a, [=(href,Link)|_], [Text]), List), % Extract the URL to the zip file from the anchor element tag
+	! ,
+	l_strip_blanks(Text, Text2) .
+
+extract_link_text([String], Text, '') :-
+	l_strip_blanks(String, Text) .
+
+
+
+
+
+
+
---
+++ tools/domainmanagement/generateinfo/tags/1.0_RC2/loadtak.pl
@@ -0,0 +1,306 @@
+:- module(loadtak, [
+	      tk_get_authorization/4,
+	      tk_get_consumer/3,
+	      tk_get_logical_address/3,
+	      tk_get_producer/4,
+	      tk_get_routing/4,
+	      tk_get_service_contract/6,
+	      tk_get_service_contract_consumers/7,
+	      tk_get_service_contract_producers/7,
+	      tk_get_tak_date/2,
+	      tk_loadtak/2 ,
+	      /*
+	      tk_get_domain_consumer/4,
+	      tk_get_domain_producer/4,
+	      tk_get_interaction/5,
+	      tk_get_tak_date/2,
+	      tk_get_tak_info/5,
+	      */
+	      tk_verify/1
+	  ]) .
+
+
+tk_loadtak(Envir, File) :- loadTAK2(Envir, File) .
+% -----------------------------------------------------------------------
+set_consumer(Envir, HSA, Desc) :-
+	tk_get_consumer(Envir, HSA, Desc),
+	! .
+set_consumer(Envir, HSA, Desc) :-
+	recordz(takConsumer, consumer(HSA, Desc, Envir)) .
+
+tk_get_consumer(Envir, HSA, Desc) :-
+	recorded(takConsumer, consumer(HSA, Desc, Envir)) .
+% -----------------------------------------------------------------------
+set_producer(Envir, HSA, DescList, Hostname) :-
+	get_producer(Envir, HSA, DescList, Hostname) ,
+	! .
+set_producer(Envir, HSA, DescList, Hostname) :-
+	recordz(takProducer, producer(HSA, DescList, Hostname, Envir)) .
+
+get_producer(Envir, HSA, DescList, Hostname) :-
+	recorded(takProducer, producer(HSA, DescList, Hostname, Envir)) .
+
+remove_producer(Envir, HSAStored, DescList, Hostname) :-
+	l_erase_all(takProducer, producer(HSAStored, DescList, Hostname, Envir)) .
+
+tk_get_producer(Envir, HSA, Desc, Hostname) :-
+	get_producer(Envir, HSA, DescList, Hostname) ,
+	atomic_list_concat(DescList, ' / ', Desc) .
+
+% -----------------------------------------------------------------------
+set_service_contract(Envir, Sc_Id, Interaction, Domain, IVersion, RivVersion) :-
+	tk_get_service_contract(Envir, Sc_Id, Interaction, Domain, IVersion, RivVersion) ,
+	! .
+
+set_service_contract(Envir, Sc_Id, Interaction, Domain, IVersion, RivVersion) :-
+	recordz(takService, service_contract(Sc_Id, Interaction, Domain, IVersion, RivVersion, Envir)) .
+
+tk_get_service_contract(Envir, Sc_Id, Interaction, Domain, IVersion, RivVersion) :-
+	recorded(takService, service_contract(Sc_Id, Interaction, Domain, IVersion, RivVersion, Envir)) .
+
+% -----------------------------------------------------------------------
+set_authorization(Envir, Sc_Id, Logical_address, ConsumerHSA) :-
+	tk_get_authorization(Envir, Sc_Id, Logical_address, ConsumerHSA) ,
+	! .
+
+set_authorization(Envir, Sc_Id, Logical_address, ConsumerHSA) :-
+	recordz(takAuth, authorization(Sc_Id, Logical_address, ConsumerHSA, Envir)) .
+
+tk_get_authorization(Envir, Sc_Id, Logical_address, ConsumerHSA) :-
+	recorded(takAuth, authorization(Sc_Id, Logical_address, ConsumerHSA, Envir)) .
+
+% -----------------------------------------------------------------------
+set_routing(Envir, Sc_Id, Logical_address, Hostname) :-
+	tk_get_routing(Envir, Sc_Id, Logical_address, Hostname) ,
+	! .
+
+set_routing(Envir, Sc_Id, Logical_address, Hostname) :-
+	recordz(takRouting, routing(Sc_Id, Logical_address, Hostname, Envir)) .
+
+tk_get_routing(Envir, Sc_Id, Logical_address, Hostname) :-
+	recorded(takRouting, routing(Sc_Id, Logical_address, Hostname, Envir)) .
+
+% -----------------------------------------------------------------------
+
+set_logical_address(Envir, LA, LADesc) :-
+	tk_get_logical_address(Envir, LA, LADesc) ,
+	! .
+
+set_logical_address(Envir, LA, LADesc) :-
+	recordz(takLA, logical_address(LA, LADesc, Envir)) .
+
+tk_get_logical_address(Envir, LA, LADesc) :-
+	recorded(takLA, logical_address(LA, LADesc, Envir)) .
+% -----------------------------------------------------------------------
+
+tk_get_service_contract_consumers(Envir, Interaction, Domain, IVersion, RivVersion, ConsumerHSA, ConsumerDesc) :-
+	findall(
+	    cons(ConsumerDesc, ConsumerHSA),
+	    (  tk_get_service_contract(Envir, Sc_Id, Interaction, Domain, IVersion, RivVersion) ,
+	       tk_get_authorization(Envir, Sc_Id, _Logical_address, ConsumerHSA) ,
+	       tk_get_consumer(Envir, ConsumerHSA, ConsumerDesc) ) ,
+	    List ) ,
+	sort(List, List2) ,
+	member(cons(ConsumerDesc, ConsumerHSA), List2) .
+
+% -----------------------------------------------------------------------
+
+% Do not use in reverse, input is contract info, out is producer info
+tk_get_service_contract_producers(Envir, Interaction, Domain, IVersion, RivVersion, ProducerDesc, Hostname) :-
+	findall(
+	    prod(ProducerDesc, Hostname) ,
+	    (   tk_get_service_contract(Envir, Sc_Id, Interaction, Domain, IVersion, RivVersion) ,
+		tk_get_routing(Envir, Sc_Id, _Logical_address, Hostname) ,
+		tk_get_producer(Envir, _ProducerHSA, ProducerDesc, Hostname) ) ,
+	    List ) ,
+	sort(List, List2) ,
+	member(prod(ProducerDesc, Hostname), List2) .
+
+
+
+% -----------------------------------------------------------------------
+
+set_tak_date(Envir, Date) :-
+	l_erase_all(takInfo, takDate(Envir, _Date)) ,
+	recordz(takInfo, takDate(Envir, Date)) .
+
+tk_get_tak_date(Envir, Date) :-
+	recorded(takInfo, takDate(Envir, Date)) .
+
+
+/* - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
+Verify the loading of the domain table
+- - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - */
+
+tk_verify(No) :-
+	setof(struct(A,B,C),
+	      tk_get_logical_address(A,B,C) ,
+	      List) ,
+	length(List, No) .
+
+% -----------------------------------------------------------------------
+tak_erase_all(Envir) :-
+	l_erase_all(takConsumer, consumer(_, _, Envir)) ,
+	l_erase_all(takProducer, producer(_, _, _, Envir)) ,
+	l_erase_all(takLA, logical_adress(_, _, Envir)) ,
+	l_erase_all(takAuth, authorization(_, _, _, Envir)) ,
+	l_erase_all(takRouting, routing(_, _, _, Envir)) ,
+	l_erase_all(takService, service_contract(_, _, _, _, _, Envir)) .
+
+% -----------------------------------------------------------------------
+
+loadTAK2(Envir, File) :-
+	nonvar(Envir) ,
+	l_counter_set(service_contract_id, 0) ,
+	l_file_date_time(File, Date, _Time),
+	set_tak_date(Envir, Date) ,
+	tak_erase_all(Envir) ,
+	l_read_file_to_list(File, [_HeaderLine | Lines], [encoding(iso_latin_1)]) ,
+	sort(Lines, Lines2),
+	loadTAK3(Envir, Lines2) .
+
+loadTAK3(_Envir, []) :- ! .
+
+loadTAK3(Envir, [Line|Rest]) :-
+	atomic_list_concat(NameList, '\t', Line) ,
+	loadTAK4(Envir, NameList) ,
+	l_write_trace(NameList, 2) ,
+	! ,
+	loadTAK3(Envir, Rest) .
+
+loadTAK3(Envir, [Line|Rest]) :-
+	l_write_trace(['tk_loadtak: could not parse line', Line], 0) ,
+	loadTAK3(Envir, Rest) .
+
+loadTAK4(Envir, [InteractionLong, InConsumerHSA, InConsumerDesc, InLogicalAddress, InLADesc, InProducerHSA, InProducerDesc, InProducerUrl]
+%        ,
+%	 consumer(ConsumerHSA, ConsumerDesc) ,
+%	 producer(ProducerHSA, ProducerDesc, Hostname) ,
+%	 logical_adress(LogicalAddress, LADesc) ,
+%	 InteractionStruct,
+%	 access(InteractionStruct, ConsumerHSA, LogicalAddress,
+%	 ProducerHSA)
+	) :-
+	l_strip_blanks(InConsumerHSA, ConsumerHSA),
+	l_strip_blanks(InConsumerDesc, ConsumerDesc),
+	l_strip_blanks(InLogicalAddress, LogicalAddress),
+	l_strip_blanks(InLADesc, LADesc),
+	l_strip_blanks(InProducerHSA, ProducerHSA),
+	l_strip_blanks(InProducerDesc, ProducerDesc),
+	l_strip_blanks(InProducerUrl, ProducerUrl),
+%	store(consumer, Envir, ConsumerHSA, ConsumerDesc) ,
+	set_consumer(Envir, ConsumerHSA, ConsumerDesc) ,
+	clean_producer_hsa(ProducerHSA, ProdHSA),
+	l_strip_blanks(ProdHSA, ProdHSA2) ,
+	l_get_hostname(ProducerUrl, Hostname) ,
+	store(producer, Envir, ProdHSA2, ProducerDesc, Hostname) ,
+	set_logical_address(Envir, LogicalAddress, LADesc) ,
+	get_interaction_info(InteractionLong, Interaction, Domain, IVersion, RivVersion) ,
+	store(service_contract, Envir, Interaction, Domain, IVersion, RivVersion, SC_Id) ,
+	set_routing(Envir, SC_Id, LogicalAddress, Hostname),
+	set_authorization(Envir, SC_Id, LogicalAddress, ConsumerHSA) ,
+	l_write_trace([Envir, ConsumerHSA, ConsumerDesc, LogicalAddress, LADesc, ProducerHSA, ProducerDesc, Hostname, Interaction, Domain, IVersion, RivVersion, SC_Id ], 3) .
+
+loadTAK4(_Envir, [InteractionLong, _ConsumerHSA, _ConsumerDesc, _LogicalAddress, _LADesc, _ProducerHSA, _ProducerDesc, _ProducerUrl]
+%	, _Cons, _Prod, _LA, _Acc
+	) :-
+	l_write_trace('Line could not be parsed', InteractionLong, 0) .
+
+% Parse: urn:riv:ehr:accesscontrol:AssertCareEngagement:1:rivtabp20
+get_interaction_info(InteractionLong, Interaction, DomainList, IVersion, RivV) :-
+	atomic_list_concat([urn, riv | InterList1], ':', InteractionLong) ,
+	% A patch to manage the faked level in Sec services domains
+	patchBifDomain(InterList1, InterList2),
+	reverse(InterList2, [RivVersion, IVersion, Interaction | DomainListRev]),
+	reverse(DomainListRev, DomainList) ,
+	get_interaction_info2(RivVersion, RivV) .
+%	l_write_trace([DomainList, Interaction, IVersion, RivV],3 ) .
+
+get_interaction_info2(rivtabp20, 20) :- ! .
+get_interaction_info2(rivtabp21, 21) :- ! .
+get_interaction_info2(Rivtabp, 00) :-
+	l_write_trace(['Error in get_interaction_info2', Rivtabp], 0) .
+
+% Store producer information
+store(producer, Envir, ProducerHSA, Desc, Hostname) :-
+	store_producer( Envir, ProducerHSA, Desc, Hostname).
+
+% Service contract
+store(service_contract, Envir, Interaction, Domain, IVersion, RivVersion, Sc_Id) :-
+	tk_get_service_contract(Envir, Sc_Id, Interaction, Domain, IVersion, RivVersion) ,
+	! .
+
+store(service_contract, Envir, Interaction, Domain, IVersion, RivVersion, Sc_Id) :-
+	% Get unique id
+	l_counter_inc(service_contract_id, Sc_Id),
+	set_service_contract(Envir, Sc_Id, Interaction, Domain, IVersion, RivVersion) .
+
+% Help predicate to store(producer clause
+store_producer(Envir, HSA, Desc, Hostname) :-
+	\+ get_producer(Envir, _HSA, _DescList, Hostname) ,
+	atom_length(Desc, Len),
+	Len > 1 ,
+	! ,
+	set_producer(Envir, HSA, [Desc], Hostname) .
+
+store_producer(Envir, HSA, Desc, Hostname) :-
+	get_producer(Envir, HSAStored, DescList, Hostname) ,
+	% Remove different postfixes of all the descriptions
+	manage_producer_descriptions(DescList, Desc, UpdatedDescList),
+	remove_producer(Envir, HSAStored, DescList, Hostname) ,
+	! ,
+	set_producer(Envir, HSA, UpdatedDescList, Hostname) .
+
+store_producer(Envir, HSA, Desc, _Hostname) :-
+	l_write_trace(['*** Error, producer with HSA= "', HSA, '" exist with description problem ("', Desc, '") in ', Envir, nl], 0).
+
+% Remove all characters after " - " sequence
+clean_producer_hsa(InHsa, OutHsa) :-
+	sub_atom(InHsa, FoundAt, _Len, _After, ' - '),
+	sub_atom(InHsa, 0, FoundAt, _After2, OutHsa).
+
+% Manage producer descriptions
+% Take a list of existing description and a new one. Try tto match the
+% new description with the existing ones, finding a common start longer
+% than (for example) 2.
+% If match, just return the list unchanged, otherwise add the new
+% description to the list and return.
+
+% No match, return the new description
+manage_producer_descriptions([], InDesc, [InDesc]) :- ! .
+
+% Got a match for item, updated the string
+manage_producer_descriptions([Desc1 | InRest], InDesc, [OutDesc | InRest]) :-
+	manage_producer_combine( Desc1, InDesc, OutDesc) ,
+	! .
+
+% No match for item, continue with next
+manage_producer_descriptions([Desc | InRest], InDesc, [Desc | OutRest]) :-
+	manage_producer_descriptions(InRest, InDesc, OutRest) .
+
+% Combine two items if the have the same prefix
+manage_producer_combine(Desc1, InDesc, NewDesc4) :-
+	l_common_prefix([Desc1, InDesc], NewDesc) ,
+	l_strip_blanks(NewDesc, NewDesc2),
+	l_strip_trailing_chars(NewDesc2, '-', NewDesc3),
+	l_strip_blanks(NewDesc3, NewDesc4),
+	atom_length(NewDesc4, Len),
+	Len > 2 .
+
+
+% Mangage error in Security services domains
+patchBifDomain([ehr,patientrelationship, _FakeLvl | Rest] ,[ehr,patientrelationship | Rest]) :- ! .
+patchBifDomain([ehr,patientconsent, _FakeLvl | Rest] ,[ehr,patientconsent | Rest]) :- ! .
+patchBifDomain(OkDomain, OkDomain).
+
+
+
+
+
+
+
+
+
+
+
+
