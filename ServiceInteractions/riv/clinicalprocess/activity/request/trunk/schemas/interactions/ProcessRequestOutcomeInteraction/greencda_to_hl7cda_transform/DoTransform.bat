f4dbff51ab0124307259a12633c60966a17aaac2
thomas.siltberg@mawell.com <thomas.siltberg@mawell.com@c1faef40-eec5-6430-f181-9cb8a2d1d0ae>
2014-10-13 15:46:35 +0000

    Updated TKB Updated AB Added IS Added code_gen Added test-suite Removed old files

--- ServiceInteractions/riv/clinicalprocess/activity/request/trunk/schemas/interactions/ProcessRequestOutcomeInteraction/greencda_to_hl7cda_transform/DoTransform.bat
+++
@@ -1,4 +0,0 @@
-@echo off
-
-AltovaXML /xslt2 "MappingMapToCDA.xslt" /in "../sample/RequestOutcomeInteraction.xml" /out "../../../../../../../../Resources/HL7 CDA/infrastructure/cda/CDA.xml" %*
-IF ERRORLEVEL 1 EXIT/B %ERRORLEVEL%
