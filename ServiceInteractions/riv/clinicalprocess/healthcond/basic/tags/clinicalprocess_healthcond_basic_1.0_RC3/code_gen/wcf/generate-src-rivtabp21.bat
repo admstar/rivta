66d9437b83096f9e50c945bc3c8b52ddfb9a8dce
Sofia.sjolen@gmail.com <Sofia.sjolen@gmail.com@c1faef40-eec5-6430-f181-9cb8a2d1d0ae>
2014-10-22 11:44:25 +0000

    missing files

---
+++ ServiceInteractions/riv/clinicalprocess/healthcond/basic/tags/clinicalprocess_healthcond_basic_1.0_RC3/code_gen/wcf/generate-src-rivtabp21.bat
@@ -0,0 +1,70 @@
+
+	@REM ---------------------------------------------------------------------------------
+	
+@REM Generates c# WCF service contracts (interface), client proxies and wcf config
+	
+@REM file for the WSDLs + XML Schemas of the service domain, using .Net WCF tool svcuti.exe
+	
+@REM ---------------------------------------------------------------------------------
+	
+@REM Licensed to the Apache Software Foundation (ASF) under one
+	
+@REM or more contributor license agreements. See the NOTICE file
+	
+@REM distributed with this work for additional information
+	
+@REM regarding copyright ownership. Inera AB licenses this file
+	@REM to you under the Apache License, Version 2.0 (the
+	@REM "License"); you may not use this file except in compliance
+	@REM with the License. You may obtain a copy of the License at
+	
+@REM
+	
+@REM http://www.apache.org/licenses/LICENSE-2.0
+	
+@REM Unless required by applicable law or agreed to in writing,
+	
+@REM software distributed under the License is distributed on an
+	
+@REM "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+	
+@REM KIND, either express or implied. See the License for the
+	
+@REM specific language governing permissions and limitations
+	
+@REM under the License.
+	
+@REM ---------------------------------------------------------------------------------
+	
+CD ..
+	
+	
+SET SCHEMADIR=schemas
+	
+	
+SET W0=%SCHEMADIR%\interactions\GetObservationInteraction\GetObservationInteraction_1.0_RIVTABP21.wsdl
+SET X0=%SCHEMADIR%\interactions\GetObservationInteraction\*.xsd
+SET W1=%SCHEMADIR%\interactions\GetMeasurementInteraction\GetMeasurementInteraction_1.0_RIVTABP21.wsdl
+SET X1=%SCHEMADIR%\interactions\GetMeasurementInteraction\*.xsd
+SET W2=%SCHEMADIR%\interactions\ProcessMeasurementInteraction\ProcesMeasurementInteraction_1.0_RIVTABP21.wsdl
+SET X2=%SCHEMADIR%\interactions\ProcessMeasurementInteraction\*.xsd
+SET W3=%SCHEMADIR%\interactions\ProcessObservationInteraction\ProcessObservationInteraction_1.0_RIVTABP21.wsdl
+SET X3=%SCHEMADIR%\interactions\ProcessObservationInteraction\*.xsd
+SET W4=%SCHEMADIR%\interactions\DeleteMeasurementInteraction\DeleteMeasurementInteraction_1.0_RIVTABP21.wsdl
+SET X4=%SCHEMADIR%\interactions\DeleteMeasurementInteraction\*.xsd
+SET W5=%SCHEMADIR%\interactions\DeleteObservationInteraction\DeleteObservationInteraction_1.0_RIVTABP21.wsdl
+SET X5=%SCHEMADIR%\interactions\DeleteObservationInteraction\*.xsd
+
+SET XCORE=%SCHEMADIR%\core_components\*.xsd
+
+SET SCHEMAS=%XCORE% %W0% %X0% %W1% %X1% %W2% %X2% %W3% %X3% %W4% %X4% %W5% %X5% 
+
+SET OUTFILE=/out:wcf\generated-src\ClinicalprocessHealthcondBasicInteractions.cs
+SET APPCONFIG=/config:wcf\generated-src\app.config
+SET NAMESPACE=/namespace:*,Riv.Clinicalprocess.Healthcond.Basic.Schemas.v1
+SET SVCUTIL="svcutil.exe"
+%SVCUTIL% /language:cs %OUTFILE% %APPCONFIG% %NAMESPACE% %SCHEMAS%
+
+CD wcf
+ECHO Generating Service contract .Net Binding interfaces and classes for clinicalprocess:healthcond:basic Release 1
+ECHO I DotNetprojektet ska du ta lagga till referens till System.ServiceModel
