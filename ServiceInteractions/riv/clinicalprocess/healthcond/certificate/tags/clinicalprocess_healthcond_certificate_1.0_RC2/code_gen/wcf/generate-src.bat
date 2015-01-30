92c6b81ddc5f158c594a8064d8f36660c027092a
johan.zetterstrom@callistaenterprise.se <johan.zetterstrom@callistaenterprise.se@c1faef40-eec5-6430-f181-9cb8a2d1d0ae>
2015-01-30 11:36:21 +0000

    klar för version 1.0

---
+++ ServiceInteractions/riv/clinicalprocess/healthcond/certificate/tags/clinicalprocess_healthcond_certificate_1.0_RC2/code_gen/wcf/generate-src.bat
@@ -0,0 +1,58 @@
+@REM ---------------------------------------------------------------------------------
+@REM Generates c# WCF service contracts (interface), client proxies and wcf config 
+@REM file for clinicalprocess:healthcond:certificate WSDLs + XML Schemas, using .Net WCF tool svcutil.exe
+@REM ---------------------------------------------------------------------------------
+@REM Licensed to the Apache Software Foundation (ASF) under one
+@REM or more contributor license agreements. See the NOTICE file
+@REM distributed with this work for additional information
+@REM regarding copyright ownership. The ASF licenses this file
+@REM to you under the Apache License, Version 2.0 (the
+@REM "License"); you may not use this file except in compliance
+@REM with the License. You may obtain a copy of the License at
+@REM 
+@REM http://www.apache.org/licenses/LICENSE-2.0
+@REM Unless required by applicable law or agreed to in writing,
+@REM software distributed under the License is distributed on an
+@REM "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+@REM KIND, either express or implied. See the License for the
+@REM specific language governing permissions and limitations
+@REM under the License.
+@REM ---------------------------------------------------------------------------------
+
+CD ../..
+SET OUTFILE=/out:wcf\generated-src\ClinicalprocessHealthcondCertificateInteractions.cs
+SET APPCONFIG=/config:wcf\generated-src\app.config
+SET NAMESPACE=/namespace:*,Riv.Clinicalprocess.Healthcond.Certificate.Schemas.v1
+SET SCHEMADIR="schemas\interactions"
+SET SVCUTIL="svcutil.exe"
+
+SET W1=%SCHEMADIR%\GetCertificateForCareInteraction\GetCertificateForCareInteraction_1.0_RIVTABP21.wsdl
+SET X1=%SCHEMADIR%\GetCertificateForCareInteraction\*.xsd
+
+SET W2=%SCHEMADIR%\ListCertificatesForCareInteraction\ListCertificatesForCareInteraction_1.0_RIVTABP21.wsdl
+SET X2=%SCHEMADIR%\ListCertificatesForCareInteraction\*.xsd
+
+SET W3=%SCHEMADIR%\ListCertificatesForCitizenInteraction\ListCertificatesForCitizenInteraction_1.0_RIVTABP21.wsdl
+SET X3=%SCHEMADIR%\ListCertificatesForCitizenInteraction\*.xsd
+
+SET W4=%SCHEMADIR%\RegisterCertificateInteraction\RegisterCertificateInteraction_1.0_RIVTABP21.wsdl
+SET X4=%SCHEMADIR%\RegisterCertificateInteraction\*.xsd
+
+SET W5=%SCHEMADIR%\ListCertificatesForCareWithQAInteraction\ListCertificatesForCareWithQAInteraction_1.0_RIVTABP21.wsdl
+SET X5=%SCHEMADIR%\ListCertificatesForCareWithQAInteraction\*.xsd
+
+SET W6=%SCHEMADIR%\CertificateStatusUpdateForCareInteraction\CertificateStatusUpdateForCareInteraction_1.0_RIVTABP21.wsdl
+SET X6=%SCHEMADIR%\CertificateStatusUpdateForCareInteraction\*.xsd
+
+SET W7=%SCHEMADIR%\CreateDraftCertificateInteraction\CreateDraftCertificateInteraction_1.0_RIVTABP21.wsdl
+SET X7=%SCHEMADIR%\CreateDraftCertificateInteraction\*.xsd
+
+SET X15=schemas\core_components\*.xsd
+
+SET SCHEMAS=%W1% %X1% %W2% %X2% %W3% %X3% %W4% %X4% %W5% %X5% %W6% %X6%  %W7% %X7% %X15%
+
+%SVCUTIL% /language:cs /wrapped %OUTFILE% %APPCONFIG% %NAMESPACE% %SCHEMAS%
+
+CD wcf
+
+ECHO I DotNetprojektet ska du ta lagga till referens till System.ServiceModel
