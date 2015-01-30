bd2a1649035f09862ec94bbe9cd52bb5c6d4946c
johan.zetterstrom@callistaenterprise.se <johan.zetterstrom@callistaenterprise.se@c1faef40-eec5-6430-f181-9cb8a2d1d0ae>
2015-01-30 09:54:38 +0000

    klar för version 1.0

--- ServiceInteractions/riv/clinicalprocess/healthcond/certificate/trunk/code_gen/wcf/generate-src.bat
+++ ServiceInteractions/riv/clinicalprocess/healthcond/certificate/trunk/code_gen/wcf/generate-src.bat
@@ -38,9 +38,18 @@ SET X3=%SCHEMADIR%\ListCertificatesForCitizenInteraction\*.xsd
 SET W4=%SCHEMADIR%\RegisterCertificateInteraction\RegisterCertificateInteraction_0.9_RIVTABP21.wsdl
 SET X4=%SCHEMADIR%\RegisterCertificateInteraction\*.xsd
 
+SET W5=%SCHEMADIR%\ListCertificatesForCareWithQAInteraction\ListCertificatesForCareWithQAInteraction_0.9_RIVTABP21.wsdl
+SET X5=%SCHEMADIR%\ListCertificatesForCareWithQAInteraction\*.xsd
+
+SET W6=%SCHEMADIR%\CertificateStatusUpdateForCareInteraction\CertificateStatusUpdateForCareInteraction_0.9_RIVTABP21.wsdl
+SET X6=%SCHEMADIR%\CertificateStatusUpdateForCareInteraction\*.xsd
+
+SET W7=%SCHEMADIR%\CreateDraftCertificateInteraction\CreateDraftCertificateInteraction_0.9_RIVTABP21.wsdl
+SET X7=%SCHEMADIR%\CreateDraftCertificateInteraction\*.xsd
+
 SET X15=schemas\core_components\*.xsd
 
-SET SCHEMAS=%W1% %X1% %W2% %X2% %W3% %X3% %W4% %X4% %X15%
+SET SCHEMAS=%W1% %X1% %W2% %X2% %W3% %X3% %W4% %X4% %W5% %X5% %W6% %X6%  %W7% %X7% %X15%
 
 %SVCUTIL% /language:cs /wrapped %OUTFILE% %APPCONFIG% %NAMESPACE% %SCHEMAS%
 
