af82f3b34a3b8696a18836b79c59ff4cc056df86
khaled.daham@carity.se <khaled.daham@carity.se@c1faef40-eec5-6430-f181-9cb8a2d1d0ae>
2015-04-02 12:43:00 +0000

    updated buildscript

--- ServiceInteractions/riv/clinicalprocess/activityprescription/prescribe/trunk/code_gen/wcf/generate-src-rivtabp21.bat
+++ ServiceInteractions/riv/clinicalprocess/activityprescription/prescribe/trunk/code_gen/wcf/generate-src-rivtabp21.bat
@@ -7,11 +7,11 @@ rem SET XSDDIR="C:\Users\plus\Google Drive\NOD Tj?nstekontrakt v2\schemas\core_c
 rem SET SCHEMADIR="C:\BACKED\HCT_VSS\INERA\NOD\wcf\schemas\interactions"
 rem SET XSDDIR="C:\BACKED\HCT_VSS\INERA\NOD\wcf\schemas\core_components"
 
-SET W0=%SCHEMADIR%\AttachMedicationDispensePrescriptionInteraction\*.wsdl
-SET X0=%SCHEMADIR%\AttachMedicationDispensePrescriptionInteraction\*.xsd
+SET W0=%SCHEMADIR%\AttachMedicationDispenseAuthorizationInteraction\*.wsdl
+SET X0=%SCHEMADIR%\AttachMedicationDispenseAuthorizationInteraction\*.xsd
 
-SET W1=%SCHEMADIR%\CancelMedicationDispensePrescriptionInteraction\*.wsdl
-SET X1=%SCHEMADIR%\CancelMedicationDispensePrescriptionInteraction\*.xsd
+SET W1=%SCHEMADIR%\CancelMedicationDispenseAuthorizationInteraction\*.wsdl
+SET X1=%SCHEMADIR%\CancelMedicationDispenseAuthorizationInteraction\*.xsd
 
 SET W2=%SCHEMADIR%\CheckMedicationListVersionInteraction\*.wsdl
 SET X2=%SCHEMADIR%\CheckMedicationListVersionInteraction\*.xsd
@@ -31,11 +31,14 @@ SET X5=%SCHEMADIR%\GetMedicationDispenseAuthorizationsInteraction\*.xsd
 SET W6=%SCHEMADIR%\GetMedicationPrescriptionsInteraction\*.wsdl
 SET X6=%SCHEMADIR%\GetMedicationPrescriptionsInteraction\*.xsd
 
-SET W7=%SCHEMADIR%\RegisterMedicationDispenseAuthorizationsInteractions\*.wsdl
-SET X7=%SCHEMADIR%\RegisterMedicationDispenseAuthorizationsInteractions\*.xsd
+SET W15=%SCHEMADIR%\PrintListOfDispensedDrugsInteraction\*.wsdl
+SET X15=%SCHEMADIR%\PrintListOfDispensedDrugsInteraction\*.xsd
 
-SET W8=%SCHEMADIR%\RegisterMedicationDispensePrescriptionWithoutPersonIdInteraction\*.wsdl
-SET X8=%SCHEMADIR%\RegisterMedicationDispensePrescriptionWithoutPersonIdInteraction\*.xsd
+SET W7=%SCHEMADIR%\RegisterMedicationDispenseAuthorizationInteraction\*.wsdl
+SET X7=%SCHEMADIR%\RegisterMedicationDispenseAuthorizationInteraction\*.xsd
+
+SET W8=%SCHEMADIR%\RegisterMedicationDispenseAuthorizationWithoutPersonIdInteraction\*.wsdl
+SET X8=%SCHEMADIR%\RegisterMedicationDispenseAuthorizationWithoutPersonIdInteraction\*.xsd
 
 SET W9=%SCHEMADIR%\RegisterMedicationPrescriptionInteraction\*.wsdl
 SET X9=%SCHEMADIR%\RegisterMedicationPrescriptionInteraction\*.xsd
@@ -55,7 +58,7 @@ SET X14=%SCHEMADIR%\PrintListOfDispensedDrugsInteraction\*.xsd
 
 SET XCORE=%XSDDIR%\*.xsd %XSDDIR%\generated\*.xsd
 
-SET SCHEMAS=%XCORE% %W0% %X0% %W1% %X1% %W2% %X2%  %W3% %X3%  %W4% %X4%  %W5% %X5%  %W6% %X6%  %W7% %X7%  %W8% %X8%  %W9% %X9%  %W10% %X10%  %W11% %X11%  %W12% %X12%  %W13% %X13% %W14% %X14%
+SET SCHEMAS=%XCORE% %W0% %X0% %W1% %X1% %W2% %X2%  %W3% %X3%  %W4% %X4%  %W5% %X5%  %W6% %X6%  %W7% %X7%  %W8% %X8%  %W9% %X9%  %W10% %X10%  %W11% %X11%  %W12% %X12%  %W13% %X13% %W14% %X14% %W15% %X15%
 
 SET OUTFILE=/out:generated-src\nod.cs
 SET APPCONFIG=/config:generated-src\app.config
